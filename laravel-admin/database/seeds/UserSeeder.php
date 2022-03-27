74ed38281fc15dad25cfafab1b2f8fcff3b81584
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-03-27 21:33:17 +0500

    Hello Route | User Seeder | fake data | debug

--- laravel-admin/database/seeds/UserSeeder.php
+++ laravel-admin/database/seeds/UserSeeder.php
@@ -12,6 +12,6 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->count(20)->create();
+        factory(App\User::class)->count(20)->create();
     }
 }
