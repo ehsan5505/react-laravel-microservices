6a9e4932accf4eb477a57cfce737a1402040ba3f
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-04 21:44:24 +0500

    User | Validation | Store  | debug

--- laravel-admin/app/Http/Controllers/UserController.php
+++ laravel-admin/app/Http/Controllers/UserController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests;
+use App\Http\Requests\UserCreateRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\User;
