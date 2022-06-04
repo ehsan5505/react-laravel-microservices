f9f6291c0b0f5cb4d65a3ea6ff5874567594b69e
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-04 16:13:33 +0500

    [User] | Update User

--- laravel-admin/app/Http/Controllers/UserController.php
+++ laravel-admin/app/Http/Controllers/UserController.php
@@ -14,14 +14,20 @@ class UserController extends Controller
         return "Hello From the Controller";
     }
 
+    // Return all the users
+    // @users
     function index(){
         return User::all();
     }
 
+    // Return Particular user
+    // @users:id
     function show($id){
         return User::find($id);
     }
 
+    // Create new user
+    // POST @users
     function create(Request $request){
         $user =  User::create([
             'first_name' => $request->first_name,
@@ -32,4 +38,17 @@ class UserController extends Controller
         
         return response($user,Response::HTTP_CREATED);
     }
+
+    // Update Particular user
+    // PUT @users:id
+    function update($id,Request $request){
+        $user = User::find($id);
+
+        return ['first_name' => $request->first_name,
+        'last_name'  => $request->last_name,
+        'email'     => $request->email,
+        'password'  => Hash::make($request->password)];
+    }
+
+
 }
