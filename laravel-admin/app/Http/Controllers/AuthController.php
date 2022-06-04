1bd37df0ba8f24d24e8950ab7c5e108f8b8dc847
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-05 00:17:52 +0500

    [Auth] | login | debug

--- laravel-admin/app/Http/Controllers/AuthController.php
+++ laravel-admin/app/Http/Controllers/AuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use Symfony\Component\HttpFoundation\Response;
 class AuthController extends Controller
 {
     // login
