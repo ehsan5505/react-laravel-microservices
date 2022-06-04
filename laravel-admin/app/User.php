5f5244a207d407082200bded659d93579e985a57
Ehsan <ehsan5505@gmail.com>
2022-06-04 21:21:23 +0500

    Commit after the Cache Refresh

---
+++ laravel-admin/app/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    protected $guarded = ['id'];
+
+    protected $hidden = [
+        'password',
+    ];
+
+   
+}
