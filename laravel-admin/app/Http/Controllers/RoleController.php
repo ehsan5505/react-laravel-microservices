12b15203c350e490fcc445b106ed65bfd25ea6a2
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-05 18:19:29 +0500

    [Role] | Role Controller | Updated

--- laravel-admin/app/Http/Controllers/RoleController.php
+++ laravel-admin/app/Http/Controllers/RoleController.php
@@ -2,62 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Role;
 use Illuminate\Http\Request;
+use Response;
 
 class RoleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        return Role::all();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $role = Role::create(['name' => $request->only('name')]);
+        return response($role,Response::HTTP_CREATED);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        //
+        $role = Role::find($id);
+        return response($role,Response::HTTP_ACCEPTED);        
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+        $role = Role::find($id);
+        $role->update($request->only('name'));
+        return response($role,Response::HTTP_ACCEPTED);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        Role::destroy($id);
+
+        return response(null,Response::HTTP_NO_CONTENT);
     }
 }
