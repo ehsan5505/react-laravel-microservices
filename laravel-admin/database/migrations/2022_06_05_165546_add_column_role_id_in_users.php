e2d548c5db4abf91741942290227633b67572131
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-05 21:59:12 +0500

    [Role] | Role_ID in Users | Alter the user table

--- laravel-admin/database/migrations/2022_06_05_165546_add_column_role_id_in_users.php
+++ laravel-admin/database/migrations/2022_06_05_165546_add_column_role_id_in_users.php
@@ -14,7 +14,8 @@ class AddColumnRoleIdInUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->unsignedBigInt('role_id');
+            $table->Foreign('role_id')->references('roles')->on('id');
         });
     }
 
@@ -26,7 +27,8 @@ class AddColumnRoleIdInUsers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropForeign(['role_id']);
+            $table->dropColumn('role_id');
         });
     }
 }
