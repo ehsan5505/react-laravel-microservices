749f791f2742b65cfae34d38464619cd1952b5e8
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-06 03:04:48 +0500

    [Products] | Product Controller | Create ImageController as separate for Image Uploading | debug

--- laravel-admin/app/Http/Requests/ImageUploadRequest.php
+++ laravel-admin/app/Http/Requests/ImageUploadRequest.php
@@ -13,7 +13,7 @@ class ImageUploadRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,7 @@ class ImageUploadRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'image' =>  'required|mime:jpg,jpeg,png,gif,webp'
         ];
     }
 }
