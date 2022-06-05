27c4319a23e09552ca4508f32ba6ba848f8760d1
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-05 23:09:59 +0500

    [Role] | Role_ID in Users | Bind the RoleId in the API Response | UserResource

--- laravel-admin/app/Http/Resources/UserResource.php
+++ laravel-admin/app/Http/Resources/UserResource.php
@@ -14,6 +14,12 @@ class UserResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id'    => $this->id,
+            'first_name'    =>  $this->first_name,
+            'last_name'     =>  $this->last_name,
+            'email'         =>  $this->email,
+            'role'          =>  $this->role
+        ]
     }
 }
