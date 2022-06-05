93644720f9bb25963d4e01473c16a3c3878aa248
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-05 22:04:50 +0500

    [Role] | Role_ID in Users | Migration | debug

--- laravel-admin/database/migrations/2022_06_05_165546_add_column_role_id_in_users.php
+++ laravel-admin/database/migrations/2022_06_05_165546_add_column_role_id_in_users.php
@@ -15,7 +15,7 @@ class AddColumnRoleIdInUsers extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->unsignedBigInteger('role_id');
-            $table->Foreign('role_id')->references('roles')->on('id');
+            $table->Foreign('role_id')->references('id')->on('roles');
         });
     }
 
