f66304cd064783b6acc705f989b130bfd9539e3c
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-05 22:29:41 +0500

    [Role] | Role_ID in Users | Update Schema | debug

--- laravel-admin/database/seeds/DatabaseSeeder.php
+++ laravel-admin/database/seeds/DatabaseSeeder.php
@@ -12,9 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(
-            RoleSeeder::class
-            // ,
-            // UserSeeder::class
+            RoleSeeder::class,
+            UserSeeder::class
         );
     }
 }
