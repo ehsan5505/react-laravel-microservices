2e75e6f21bfbdf1ceb7eb185ff9a833b0b9d488f
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-06 16:28:38 +0500

    [Orders] | Export CSV file | debug

--- laravel-admin/app/Http/Controllers/OrderController.php
+++ laravel-admin/app/Http/Controllers/OrderController.php
@@ -49,6 +49,6 @@ class OrderController extends Controller
 
         }
 
-        Response::stream(($callback,Response::HTTP_ACCEIPTED,$headers);
+        return Response::stream(($callback,Response::HTTP_ACCEIPTED,$headers);
     }
 }
