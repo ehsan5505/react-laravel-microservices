2e15ee87b920932d41262084dd114f1c0fcd286f
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-06 18:05:52 +0500

    [Permission] | Seeder | Role Permission Seeder | debug

--- laravel-admin/database/seeds/RolePermissionSeeder.php
+++ laravel-admin/database/seeds/RolePermissionSeeder.php
@@ -26,7 +26,7 @@ class RolePermissionSeeder extends Seeder
         foreach($permissions as $permission)
         {
             if(!in_array($permission,['edit_roles'])){
-                DB:table('role_permission')->insert([
+                DB::table('role_permission')->insert([
                     'role_id'           =>  $editor->id,
                     'permission_id'     =>  $permission->id,
                 ]);
