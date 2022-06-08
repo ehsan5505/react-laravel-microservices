8bf588eeeed19730c64e309820ccb724685d54e5
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-08 19:01:31 +0500

    [User] | Role Name is wrong

--- laravel-admin/app/User.php
+++ laravel-admin/app/User.php
@@ -53,7 +53,7 @@ class User extends Authenticatable
     ];
 
     public function role(){
-        return $this->belongsTo(Role::class,'role_id');
+        return $this->belongsTo(Role::class);
     }
 
     public function permissions(){
