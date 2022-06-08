140a7bee85cf2b34ff679da26e7c3c149c0f7057
Ehsan <ehsan5505@gmail.com>
2022-06-08 17:48:57 +0500

    [Permission] | Add Resource and Controller

--- laravel-admin/app/Role.php
+++ laravel-admin/app/Role.php
@@ -15,6 +15,8 @@ use Illuminate\Database\Eloquent\Model;
  * @method static \Illuminate\Database\Eloquent\Builder|Role whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Role whereName($value)
  * @mixin \Eloquent
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Permission[] $permissions
+ * @property-read int|null $permissions_count
  */
 class Role extends Model
 {
