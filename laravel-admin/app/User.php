8cde6b59770c1d603f73d585e9669d2c60dbb7db
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-08 17:03:14 +0500

    [User | Link the Permission using Role

--- laravel-admin/app/User.php
+++ laravel-admin/app/User.php
@@ -56,5 +56,9 @@ class User extends Authenticatable
         return $this->belongsTo(Role::class);
     }
 
+    public function permissions(){
+        return $this->role->permissions;
+    }
+
    
 }
