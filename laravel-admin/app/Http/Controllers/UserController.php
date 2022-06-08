100002a672421db960152d97bd4b1cf8c2c8d884
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-08 17:08:06 +0500

    [User | Link the Permission using Role | debug

--- laravel-admin/app/Http/Controllers/UserController.php
+++ laravel-admin/app/Http/Controllers/UserController.php
@@ -31,7 +31,7 @@ class UserController extends Controller
     function show($id){
         $user = \Auth::user();
         return (new UserResource(User::find($id)))->additional([
-            'data','permissions'=>$user->permissions
+            'data','permissions'=>'Yes'
         ]);
     }
 
