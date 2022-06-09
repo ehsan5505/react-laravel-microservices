b746126c6e913e46e6eef40854544477321ab9f6
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-09 15:46:50 +0500

    [Dashboard] | Chart for the total order each day | debug

--- laravel-admin/app/Http/Controllers/DashboardController.php
+++ laravel-admin/app/Http/Controllers/DashboardController.php
@@ -19,7 +19,7 @@ class DashboardController extends Controller
         ->groupBy('day')
         ->get();
         
-        return $orders;
+        return response($orders,Response::HTTP_ACCEPTED);
         // return response($orders,Response::HTTP_ACCEPTED);
     }
 }
