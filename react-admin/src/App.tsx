f18f9923e588210efd5fdd6918d0fe4ba00ce9d0
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-10 04:50:07 +0500

    [React] | Bootstrap Setup

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -1,111 +1,110 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div class="container-fluid">
-        <div class="row">
+      <div className="container-fluid">
+        <div className="row">
           <nav
             id="sidebarMenu"
-            class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
+            className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
           >
-            <div class="position-sticky pt-3">
-              <ul class="nav flex-column">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">
-                    <span data-feather="home" class="align-text-bottom"></span>
+            <div className="position-sticky pt-3">
+              <ul className="nav flex-column">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    <span data-feather="home" className="align-text-bottom"></span>
                     Dashboard
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
-                    <span data-feather="file" class="align-text-bottom"></span>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    <span data-feather="file" className="align-text-bottom"></span>
                     Orders
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
                     <span
                       data-feather="shopping-cart"
-                      class="align-text-bottom"
+                      className="align-text-bottom"
                     ></span>
                     Products
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
-                    <span data-feather="users" class="align-text-bottom"></span>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    <span data-feather="users" className="align-text-bottom"></span>
                     Customers
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
                     <span
                       data-feather="bar-chart-2"
-                      class="align-text-bottom"
+                      className="align-text-bottom"
                     ></span>
                     Reports
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
                     <span
                       data-feather="layers"
-                      class="align-text-bottom"
+                      className="align-text-bottom"
                     ></span>
                     Integrations
                   </a>
                 </li>
               </ul>
 
-              <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
+              <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
                 <span>Saved reports</span>
                 <a
-                  class="link-secondary"
+                  className="link-secondary"
                   href="#"
                   aria-label="Add a new report"
                 >
                   <span
                     data-feather="plus-circle"
-                    class="align-text-bottom"
+                    className="align-text-bottom"
                   ></span>
                 </a>
               </h6>
-              <ul class="nav flex-column mb-2">
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
+              <ul className="nav flex-column mb-2">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
                     <span
                       data-feather="file-text"
-                      class="align-text-bottom"
+                      className="align-text-bottom"
                     ></span>
                     Current month
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
                     <span
                       data-feather="file-text"
-                      class="align-text-bottom"
+                      className="align-text-bottom"
                     ></span>
                     Last quarter
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
                     <span
                       data-feather="file-text"
-                      class="align-text-bottom"
+                      className="align-text-bottom"
                     ></span>
                     Social engagement
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
                     <span
                       data-feather="file-text"
-                      class="align-text-bottom"
+                      className="align-text-bottom"
                     ></span>
                     Year-end sale
                   </a>
@@ -114,31 +113,31 @@ function App() {
             </div>
           </nav>
 
-          <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-              <h1 class="h2">Dashboard</h1>
-              <div class="btn-toolbar mb-2 mb-md-0">
-                <div class="btn-group me-2">
+          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+              <h1 className="h2">Dashboard</h1>
+              <div className="btn-toolbar mb-2 mb-md-0">
+                <div className="btn-group me-2">
                   <button
                     type="button"
-                    class="btn btn-sm btn-outline-secondary"
+                    className="btn btn-sm btn-outline-secondary"
                   >
                     Share
                   </button>
                   <button
                     type="button"
-                    class="btn btn-sm btn-outline-secondary"
+                    className="btn btn-sm btn-outline-secondary"
                   >
                     Export
                   </button>
                 </div>
                 <button
                   type="button"
-                  class="btn btn-sm btn-outline-secondary dropdown-toggle"
+                  className="btn btn-sm btn-outline-secondary dropdown-toggle"
                 >
                   <span
                     data-feather="calendar"
-                    class="align-text-bottom"
+                    className="align-text-bottom"
                   ></span>
                   This week
                 </button>
@@ -146,15 +145,15 @@ function App() {
             </div>
 
             <canvas
-              class="my-4 w-100"
+              className="my-4 w-100"
               id="myChart"
               width="900"
               height="380"
             ></canvas>
 
             <h2>Section title</h2>
-            <div class="table-responsive">
-              <table class="table table-striped table-sm">
+            <div className="table-responsive">
+              <table className="table table-striped table-sm">
                 <thead>
                   <tr>
                     <th scope="col">#</th>
