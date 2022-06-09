65fa7ae5108413cd5e58da2d1002ae96f9227f03
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-09 15:52:53 +0500

    [Dashboard] | Chart for the total order each day | debug

--- laravel-admin/app/Http/Controllers/DashboardController.php
+++ laravel-admin/app/Http/Controllers/DashboardController.php
@@ -14,9 +14,9 @@ class DashboardController extends Controller
 
         $orders = Order::query()
         ->join("order_items","order_items.order_id", "=", "orders.id")
-        ->selectRaw("DATE_FORMAT(orders.created_at, '%Y-%m-%d') as day,orders.id as OrderId,order_items.id as orderItemId")
+        ->selectRaw("DATE_FORMAT(orders.created_at, '%Y-%m-%d') as daya,orders.id as OrderId,order_items.id as orderItemId")
         // ->selectRaw("DATE_FORMAT(orders.created_at,'%Y-%m-%d') as date,sum(order_items.quantity*order_items*price) as sum")
-        ->groupBy('day')
+        ->groupBy('daya')
         ->get();
         
         return response($orders,Response::HTTP_ACCEPTED);
