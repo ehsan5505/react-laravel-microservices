da00e91935045422bbbf1cad7abe0cb753641c00
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-10 17:37:05 +0500

    [React] | Compoent | Register | change template

--- react-admin/src/public/Register.tsx
+++ react-admin/src/public/Register.tsx
@@ -1,136 +1,106 @@
 import React from "react";
-import './Register.css';
+import "./Register.css";
 
 const Register = () => (
-  <section className="text-center text-lg-start">
-    
-    <div className="container py-4">
-      <div className="row g-0 align-items-center">
-        <div className="col-lg-6 mb-5 mb-lg-0">
-          <div
-            className="card cascading-right"
-            style={{
-              background: "hsla(0, 0%, 100%, 0.55)",
-              backdropFilter: "blur(30px)",
-            }}
-          >
-            <div className="card-body p-5 shadow-5 text-center">
-              <h2 className="fw-bold mb-5">Sign up now</h2>
-              <div className="row">
-                <div className="col-md-6 mb-4">
-                  <div className="htmlForm-outline">
+  <section className="">
+    <div
+      className="px-4 py-5 px-md-5 text-center text-lg-start"
+      style={{ backgroundColor: "hsl(0, 0%, 96%)" }}
+    >
+      <div className="container">
+        <div className="row gx-lg-5 align-items-center">
+          <div className="col-lg-6 mb-5 mb-lg-0">
+            <h1 className="my-5 display-3 fw-bold ls-tight">
+              The best offer <br />
+              <span className="text-primary">for your business</span>
+            </h1>
+            <p style={{ color: "hsl(217, 10%, 50.8%)" }}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
+              itaque accusantium odio, soluta, corrupti aliquam quibusdam
+              tempora at cupiditate quis eum maiores libero veritatis? Dicta
+              facilis sint aliquid ipsum atque?
+            </p>
+          </div>
+
+          <div className="col-lg-6 mb-5 mb-lg-0">
+            <div className="card">
+              <div className="card-body py-5 px-md-5">
+                <form>
+                  <div className="row">
+                    <div className="col-md-6 mb-4">
+                      <div className="form-outline">
+                        <input
+                          type="text"
+                          id="form3Example1"
+                          className="form-control"
+                        />
+                        <label className="form-label" htmlFor="form3Example1">
+                          First name
+                        </label>
+                      </div>
+                    </div>
+                    <div className="col-md-6 mb-4">
+                      <div className="form-outline">
+                        <input
+                          type="text"
+                          id="form3Example2"
+                          className="form-control"
+                        />
+                        <label className="form-label" htmlFor="form3Example2">
+                          Last name
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="form-outline mb-4">
                     <input
-                      type="text"
-                      id="htmlForm3Example1"
-                      className="htmlForm-control"
+                      type="email"
+                      id="form3Example3"
+                      className="form-control"
                     />
-                    <label
-                      className="htmlForm-label"
-                      htmlFor="htmlForm3Example1"
-                    >
-                      First name
+                    <label className="form-label" htmlFor="form3Example3">
+                      Email address
                     </label>
                   </div>
-                </div>
-                <div className="col-md-6 mb-4">
-                  <div className="htmlForm-outline">
+
+                  <div className="form-outline mb-4">
                     <input
-                      type="text"
-                      id="htmlForm3Example2"
-                      className="htmlForm-control"
+                      type="password"
+                      id="form3Example4"
+                      className="form-control"
                     />
-                    <label
-                      className="htmlForm-label"
-                      htmlFor="htmlForm3Example2"
-                    >
-                      Last name
+                    <label className="form-label" htmlFor="form3Example4">
+                      Password
                     </label>
                   </div>
-                </div>
-              </div>
 
-              <div className="htmlForm-outline mb-4">
-                <input
-                  type="email"
-                  id="htmlForm3Example3"
-                  className="htmlForm-control"
-                />
-                <label className="htmlForm-label" htmlFor="htmlForm3Example3">
-                  Email address
-                </label>
-              </div>
+                  <div className="form-check d-flex justify-content-center mb-4">
+                    <input
+                      className="form-check-input me-2"
+                      type="checkbox"
+                      value=""
+                      id="form2Example33"
+                      checked
+                    />
+                    <label
+                      className="form-check-label"
+                      htmlFor="form2Example33"
+                    >
+                      Subscribe to our newsletter
+                    </label>
+                  </div>
 
-              <div className="htmlForm-outline mb-4">
-                <input
-                  type="password"
-                  id="htmlForm3Example4"
-                  className="htmlForm-control"
-                />
-                <label className="htmlForm-label" htmlFor="htmlForm3Example4">
-                  Password
-                </label>
-              </div>
-
-              <div className="htmlForm-check d-flex justify-content-center mb-4">
-                <input
-                  className="htmlForm-check-input me-2"
-                  type="checkbox"
-                  value=""
-                  id="htmlForm2Example33"
-                  checked
-                />
-                <label
-                  className="htmlForm-check-label"
-                  htmlFor="htmlForm2Example33"
-                >
-                  Subscribe to our newsletter
-                </label>
-              </div>
-
-              <button type="submit" className="btn btn-primary btn-block mb-4">
-                Sign up
-              </button>
-
-              <div className="text-center">
-                <p>or sign up with:</p>
-                <button
-                  type="button"
-                  className="btn btn-link btn-floating mx-1"
-                >
-                  <i className="fab fa-facebook-f"></i>
-                </button>
-
-                <button
-                  type="button"
-                  className="btn btn-link btn-floating mx-1"
-                >
-                  <i className="fab fa-google"></i>
-                </button>
-
-                <button
-                  type="button"
-                  className="btn btn-link btn-floating mx-1"
-                >
-                  <i className="fab fa-twitter"></i>
-                </button>
-
-                <button
-                  type="button"
-                  className="btn btn-link btn-floating mx-1"
-                >
-                  <i className="fab fa-github"></i>
-                </button>
+                  <button
+                    type="submit"
+                    className="btn btn-primary btn-block mb-4"
+                  >
+                    Sign up
+                  </button>
+                </form>
               </div>
             </div>
           </div>
-        </div>
-
-        <div className="col-lg-6 mb-5 mb-lg-0">
-          <img
-            src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg"
-            className="w-100 rounded-4 shadow-4"
-            alt=""
-          />
         </div>
       </div>
     </div>
