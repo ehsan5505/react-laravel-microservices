c0f806e41703de69036fbf58315b25ecb6aead79
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-10 17:31:01 +0500

    [React] | Solve the warnings

--- react-admin/src/secure/Wrapper.tsx
+++ react-admin/src/secure/Wrapper.tsx
@@ -2,31 +2,27 @@ import React, { Component } from "react";
 import Menu from "./components/menu";
 import Nav from "./components/nav";
 
-
-interface WrapperProps
-{
-  children: React.ReactNode
+interface WrapperProps {
+  children: React.ReactNode;
 }
 
 class Wrapper extends Component<WrapperProps> {
   render() {
-    {
-      return (
-        <>
-          <Nav />
+    return (
+      <>
+        <Nav />
 
-          <div className="container-fluid">
-            <div className="row">
-              <Menu />
+        <div className="container-fluid">
+          <div className="row">
+            <Menu />
 
-              <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                {this.props.children}
-              </main>
-            </div>
+            <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+              {this.props.children}
+            </main>
           </div>
-        </>
-      );
-    }
+        </div>
+      </>
+    );
   }
 }
 
