13f6526291976d932d210f102d184bd6bf73866a
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-10 05:00:36 +0500

    [React] | Bootstrap Setup | Dashboard CSS

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -4,6 +4,36 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+        <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">
+          Company name
+        </a>
+        <button
+          className="navbar-toggler position-absolute d-md-none collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#sidebarMenu"
+          aria-controls="sidebarMenu"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <input
+          className="form-control form-control-dark w-100 rounded-0 border-0"
+          type="text"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <div className="navbar-nav">
+          <div className="nav-item text-nowrap">
+            <a className="nav-link px-3" href="#">
+              Sign out
+            </a>
+          </div>
+        </div>
+      </header>
+
       <div className="container-fluid">
         <div className="row">
           <nav
@@ -14,13 +44,19 @@ function App() {
               <ul className="nav flex-column">
                 <li className="nav-item">
                   <a className="nav-link active" aria-current="page" href="#">
-                    <span data-feather="home" className="align-text-bottom"></span>
+                    <span
+                      data-feather="home"
+                      className="align-text-bottom"
+                    ></span>
                     Dashboard
                   </a>
                 </li>
                 <li className="nav-item">
                   <a className="nav-link" href="#">
-                    <span data-feather="file" className="align-text-bottom"></span>
+                    <span
+                      data-feather="file"
+                      className="align-text-bottom"
+                    ></span>
                     Orders
                   </a>
                 </li>
@@ -35,7 +71,10 @@ function App() {
                 </li>
                 <li className="nav-item">
                   <a className="nav-link" href="#">
-                    <span data-feather="users" className="align-text-bottom"></span>
+                    <span
+                      data-feather="users"
+                      className="align-text-bottom"
+                    ></span>
                     Customers
                   </a>
                 </li>
@@ -169,6 +208,111 @@ function App() {
                     <td>random</td>
                     <td>data</td>
                     <td>placeholder</td>
+                    <td>text</td>
+                  </tr>
+                  <tr>
+                    <td>1,002</td>
+                    <td>placeholder</td>
+                    <td>irrelevant</td>
+                    <td>visual</td>
+                    <td>layout</td>
+                  </tr>
+                  <tr>
+                    <td>1,003</td>
+                    <td>data</td>
+                    <td>rich</td>
+                    <td>dashboard</td>
+                    <td>tabular</td>
+                  </tr>
+                  <tr>
+                    <td>1,003</td>
+                    <td>information</td>
+                    <td>placeholder</td>
+                    <td>illustrative</td>
+                    <td>data</td>
+                  </tr>
+                  <tr>
+                    <td>1,004</td>
+                    <td>text</td>
+                    <td>random</td>
+                    <td>layout</td>
+                    <td>dashboard</td>
+                  </tr>
+                  <tr>
+                    <td>1,005</td>
+                    <td>dashboard</td>
+                    <td>irrelevant</td>
+                    <td>text</td>
+                    <td>placeholder</td>
+                  </tr>
+                  <tr>
+                    <td>1,006</td>
+                    <td>dashboard</td>
+                    <td>illustrative</td>
+                    <td>rich</td>
+                    <td>data</td>
+                  </tr>
+                  <tr>
+                    <td>1,007</td>
+                    <td>placeholder</td>
+                    <td>tabular</td>
+                    <td>information</td>
+                    <td>irrelevant</td>
+                  </tr>
+                  <tr>
+                    <td>1,008</td>
+                    <td>random</td>
+                    <td>data</td>
+                    <td>placeholder</td>
+                    <td>text</td>
+                  </tr>
+                  <tr>
+                    <td>1,009</td>
+                    <td>placeholder</td>
+                    <td>irrelevant</td>
+                    <td>visual</td>
+                    <td>layout</td>
+                  </tr>
+                  <tr>
+                    <td>1,010</td>
+                    <td>data</td>
+                    <td>rich</td>
+                    <td>dashboard</td>
+                    <td>tabular</td>
+                  </tr>
+                  <tr>
+                    <td>1,011</td>
+                    <td>information</td>
+                    <td>placeholder</td>
+                    <td>illustrative</td>
+                    <td>data</td>
+                  </tr>
+                  <tr>
+                    <td>1,012</td>
+                    <td>text</td>
+                    <td>placeholder</td>
+                    <td>layout</td>
+                    <td>dashboard</td>
+                  </tr>
+                  <tr>
+                    <td>1,013</td>
+                    <td>dashboard</td>
+                    <td>irrelevant</td>
+                    <td>text</td>
+                    <td>visual</td>
+                  </tr>
+                  <tr>
+                    <td>1,014</td>
+                    <td>dashboard</td>
+                    <td>illustrative</td>
+                    <td>rich</td>
+                    <td>data</td>
+                  </tr>
+                  <tr>
+                    <td>1,015</td>
+                    <td>random</td>
+                    <td>tabular</td>
+                    <td>information</td>
                     <td>text</td>
                   </tr>
                 </tbody>
