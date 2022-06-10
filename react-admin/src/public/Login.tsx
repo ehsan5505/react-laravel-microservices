8a09a8c8c4eefc9db57f9eae38b9f2128e273a97
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-10 16:49:21 +0500

    [React] | Compoent | Login | restore

--- react-admin/src/public/Login.tsx
+++ react-admin/src/public/Login.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./Login";
+import './Login';
 
 class Login extends Component {
   render() {
@@ -10,7 +10,7 @@ class Login extends Component {
             <div className="col-12 col-md-8 col-lg-6 col-xl-5">
               <div
                 className="card bg-dark text-white"
-                style={{ borderRadius: "1rem" }}
+                style={{borderRadius:"1rem"}}
               >
                 <div className="card-body p-5 text-center">
                   <div className="mb-md-5 mt-md-4 pb-5">
@@ -24,8 +24,10 @@ class Login extends Component {
                         type="email"
                         id="typeEmailX"
                         className="form-control form-control-lg"
-                        placeholder="Email"
                       />
+                      <label className="form-label" htmlFor="typeEmailX">
+                        Email
+                      </label>
                     </div>
 
                     <div className="form-outline form-white mb-4">
@@ -33,9 +35,17 @@ class Login extends Component {
                         type="password"
                         id="typePasswordX"
                         className="form-control form-control-lg"
-                        placeholder="Password"
                       />
+                      <label className="form-label" htmlFor="typePasswordX">
+                        Password
+                      </label>
                     </div>
+
+                    <p className="small mb-5 pb-lg-2">
+                      <a className="text-white-50" href="#!">
+                        Forgot password?
+                      </a>
+                    </p>
 
                     <button
                       className="btn btn-outline-light btn-lg px-5"
@@ -44,14 +54,26 @@ class Login extends Component {
                       Login
                     </button>
 
-                    <div>
-                      <p className="mb-0">
-                        Don't have an account?{" "}
-                        <a href="#!" className="text-white-50 fw-bold">
-                          Sign Up
-                        </a>
-                      </p>
+                    <div className="d-flex justify-content-center text-center mt-4 pt-1">
+                      <a href="#!" className="text-white">
+                        <i className="fab fa-facebook-f fa-lg"></i>
+                      </a>
+                      <a href="#!" className="text-white">
+                        <i className="fab fa-twitter fa-lg mx-4 px-2"></i>
+                      </a>
+                      <a href="#!" className="text-white">
+                        <i className="fab fa-google fa-lg"></i>
+                      </a>
                     </div>
+                  </div>
+
+                  <div>
+                    <p className="mb-0">
+                      Don't have an account?{" "}
+                      <a href="#!" className="text-white-50 fw-bold">
+                        Sign Up
+                      </a>
+                    </p>
                   </div>
                 </div>
               </div>
