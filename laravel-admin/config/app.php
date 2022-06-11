8d1098ea647aeabc40eb027953bc6bc137ab6c6a
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-11 05:42:32 +0500

    [Laravel] | Register the CORS in the middleware

--- laravel-admin/config/app.php
+++ laravel-admin/config/app.php
@@ -174,6 +174,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        // For the CORS Service Provider
+        FruitCake\Cors\CorsServiceProvider::class,
 
     ],
 
