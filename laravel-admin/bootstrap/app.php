f390cd2944c2d7a05c90089f78a2c65d133930bd
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-11 06:20:23 +0500

    [Laravel] | Register the CORS in the middleware | debug

--- laravel-admin/bootstrap/app.php
+++ laravel-admin/bootstrap/app.php
@@ -51,5 +51,9 @@ $app->singleton(
 | from the actual running of the application and sending responses.
 |
 */
+// Headers
+header('Access-Control-Allow-Origin', '*');
+header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
 return $app;
