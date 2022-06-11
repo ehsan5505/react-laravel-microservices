f367762b2f30091a30df0a61db65d2f5e12b13e0
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-11 07:20:57 +0500

    [Laravel] | Register the CORS in the middleware | debug

--- react-admin/src/index.tsx
+++ react-admin/src/index.tsx
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 
+axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
+
 // axios.defaults.baseURL = "http://192.168.22.138:8000/api/";
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 // axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
