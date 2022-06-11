53838d64f45baceceace49b512dc0f0320af9aa0
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-11 06:46:31 +0500

    [Laravel] | Register the CORS in the middleware | debug

--- laravel-admin/app/Http/Middleware/Authenticate.php
+++ laravel-admin/app/Http/Middleware/Authenticate.php
@@ -1,9 +1,8 @@
 <?php
 
 namespace App\Http\Middleware;
-
-use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Closure;
 
 class Authenticate extends Middleware
