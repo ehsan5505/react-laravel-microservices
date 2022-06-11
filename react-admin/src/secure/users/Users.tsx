0a54ad02d1f23efe31c4ba01ae1cddc5a54a6194
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-11 22:46:18 +0500

    [React] | Users | layout

---
+++ react-admin/src/secure/users/Users.tsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import Wrapper from "../Wrapper";
+
+interface UserProps {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  role: any;
+}
+
+class User extends Component<UserProps> {
+  state = {
+    users: [],
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <h2>Section title</h2>
+        <div className="table-responsive">
+          <table className="table table-striped table-sm">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Role</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.users.map((user: UserProps) => {
+                return (
+                  <tr>
+                    <td>{user.id}</td>
+                    <td>
+                      {user.first_name} {user.last_name}
+                    </td>
+                    <td>{user.email}</td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </Wrapper>
+    );
+  }
+}
+// = () => (
+//   <Wrapper>
+//     <h2>Welcome To the User Portal...</h2>
+//   </Wrapper>
+// );
+
+export default User;
