0476adfe272ac36f686288a25788f18c5486c11d
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-11 21:12:19 +0500

    [Laravel] | Login | Debug

--- react-admin/src/public/Login.tsx
+++ react-admin/src/public/Login.tsx
@@ -14,14 +14,15 @@ class Login extends Component {
   submit = async (e: SyntheticEvent) => {
     // Prevent the Default Browser Behaviour to refresh the page on data to view
     e.preventDefault();
-    const resp = await axios.post("http://192.168.22.138:8000/api/login", {
-      email: this.email,
-      password: this.password,
-    });
-    localStorage.setItem("token", resp.data);
-    this.setState({
-      redirect: true,
-    });
+    console.info("email: "+this.email+" | password: "+this.password);
+    // const resp = await axios.post("http://192.168.22.138:8000/api/login", {
+    //   email: this.email,
+    //   password: this.password,
+    // });
+    // localStorage.setItem("token", resp.data);
+    // this.setState({
+    //   redirect: true,
+    // });
   };
 
   render() {
