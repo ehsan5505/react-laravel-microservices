e6322914848cf0c8e038fad79e25db4a6796e1eb
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-11 22:06:06 +0500

    [Axios] | | revert back to server IP

--- react-admin/src/index.tsx
+++ react-admin/src/index.tsx
@@ -7,7 +7,7 @@ import axios from 'axios';
 
 axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
 
-axios.defaults.baseURL = "http://localhost:8000/api/";
+axios.defaults.baseURL = "http://192.168.22.138:8000/api/";
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
 
