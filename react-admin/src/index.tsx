e0cd747ac911f40625a7d791eb355e36ee48c83a
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-11 22:04:35 +0500

    [Axios] | Change BaseURL to localhost

--- react-admin/src/index.tsx
+++ react-admin/src/index.tsx
@@ -7,7 +7,7 @@ import axios from 'axios';
 
 axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
 
-axios.defaults.baseURL = "http://locahost:8000/api/";
+axios.defaults.baseURL = "http://localhost:8000/api/";
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
 
