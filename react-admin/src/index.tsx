a6d0ae12b0f15e98035e023d6d1058bf7597d7f9
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-11 06:04:02 +0500

    [Laravel] | Register the CORS in the middleware | debug

--- react-admin/src/index.tsx
+++ react-admin/src/index.tsx
@@ -6,8 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 
 axios.defaults.baseURL = "http://192.168.22.138/api/";
-axios.defaults.headers["content-type"] = "application/json";
-axios.defaults.headers.common.authorization = `Bearer ${localStorage.getItem('token')}`;
+// axios.defaults.headers["content-type"] = "application/json";
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
