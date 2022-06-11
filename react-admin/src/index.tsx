05d7132097d44f588a0ec99583fe6e7bdc04aeac
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-11 05:11:32 +0500

    [React] | API | Login interact | Axios Default | debug

--- react-admin/src/index.tsx
+++ react-admin/src/index.tsx
@@ -6,8 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 
 axios.defaults.baseURL = "http://192.168.22.138/api/";
-axios.defaults.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
-
+axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
