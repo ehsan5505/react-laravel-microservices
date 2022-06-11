92d6f48e62baf07764ea87ab7817f40c762a195c
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-11 23:51:12 +0500

    [React] | Menu | Update

--- react-admin/src/secure/components/menu.tsx
+++ react-admin/src/secure/components/menu.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Dashboard from "../dashboard/Dashboard";
+import User from "../users/Users";
 
 const Menu = () => (
   <nav
@@ -8,10 +10,16 @@ const Menu = () => (
     <div className="position-sticky pt-3">
       <ul className="nav flex-column">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">
+          <NavLink to={User} className="nav-link active" aria-current="page" href="#">
             <span data-feather="home" className="align-text-bottom"></span>
             Dashboard
-          </a>
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to={Dashboard} className="nav-link active" aria-current="page" href="#">
+            <span data-feather="home" className="align-text-bottom"></span>
+            Users
+          </NavLink>
         </li>
       </ul>
     </div>
