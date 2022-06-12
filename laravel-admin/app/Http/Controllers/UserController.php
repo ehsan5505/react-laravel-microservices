8241ce62c69c53d2f8425f06e413d9e6cde4bdf8
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-12 14:50:30 +0500

    [Laravel] | User Delete | Debug

--- laravel-admin/app/Http/Controllers/UserController.php
+++ laravel-admin/app/Http/Controllers/UserController.php
@@ -70,10 +70,6 @@ class UserController extends Controller
     function destroy($id)
     {
         Gate::authorize('edit','users');
-        if (!User::find($id))
-        {
-            return response("User Not Exist", Response::HTTP_NOT_FOUND);
-        }
         User::destroy($id);
         return response(null, Response::HTTP_NO_CONTENT);
     }
