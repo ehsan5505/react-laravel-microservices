c505b3f661876d43c6b235a30b759252f07988af
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-14 03:00:48 +0500

    [Laravel] | Permission | Return the permission id as well as name

--- react-admin/src/secure/roles/Create.tsx
+++ react-admin/src/secure/roles/Create.tsx
@@ -12,24 +12,23 @@ class CreateRole extends Component {
   };
 
   check = (id: number) => {
-    console.log(`ID given in the argunment ${id}`);
-    // if (this.selected.filter((s) => s === id).length > 0) {
+    if (this.selected.filter((s) => s === id).length > 0) {
     // if the id already exist then return the exclusive (not selected ones)
-    // this.selected = this.selected.filter((s) => s !== id);
+    this.selected = this.selected.filter((s) => s !== id);
     // console.error(this.selected);
-    // return;
-    // }
+    return;
+    }
     // console.info(this.selected);
-    // this.selected.push(id);
-    // console.info(this.selected);
+    this.selected.push(id);
+    console.info(this.selected);
   };
 
   submit = async (e: SyntheticEvent) => {
     e.preventDefault();
-    // console.log({
-    //   name: this.name,
-    //   permissions: this.selected,
-    // });
+    console.log({
+      name: this.name,
+      permissions: this.selected,
+    });
   };
 
   componentDidMount = async () => {
@@ -62,7 +61,6 @@ class CreateRole extends Component {
           </div>
 
           {this.state.permissions.map((p: any) => {
-            console.info(p);
             return (
               <div className="form-check form-check-inline">
                 <input
