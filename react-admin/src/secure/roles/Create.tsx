85f72bc442a75e0768be39d2306901a1fdd7c9cd
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-14 00:25:31 +0500

    [React] | Role | Create | fetch the permissions | debug

--- react-admin/src/secure/roles/Create.tsx
+++ react-admin/src/secure/roles/Create.tsx
@@ -14,6 +14,7 @@ class CreateRole extends Component {
   componentDidMount = async () => {
     const response = await axios.get("permissions");
     this.state.permissions = response.data.data;
+    console.info(response.data.data);
   };
 
   render() {
