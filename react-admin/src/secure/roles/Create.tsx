93a16defc68b057a7d321f14d179751036dbf72a
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-14 00:22:37 +0500

    [React] | Role | Create | fetch the permissions

--- react-admin/src/secure/roles/Create.tsx
+++ react-admin/src/secure/roles/Create.tsx
@@ -1,11 +1,19 @@
+import axios from "axios";
 import React, { Component } from "react";
+import PermissionProps from "../classes/permission";
 import Wrapper from "../Wrapper";
 
 class CreateRole extends Component {
   name = "";
   selected: number[] = [];
   state = {
+    permissions: [],
     redirect: false,
+  };
+
+  componentDidMount = async () => {
+    const response = await axios.get("permissions");
+    this.state.permissions = response.data.data;
   };
 
   render() {
@@ -30,36 +38,18 @@ class CreateRole extends Component {
             </label>
           </div>
 
-          <div className="form-check form-check-inline">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value="option1"
-            />
-            <label className="form-check-label" htmlFor="inlineCheckbox1">
-              1
-            </label>
-          </div>
-          <div className="form-check form-check-inline">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value="option2"
-            />
-            <label className="form-check-label" htmlFor="inlineCheckbox2">
-              2
-            </label>
-          </div>
-          <div className="form-check form-check-inline">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value="option3"
-            />
-            <label className="form-check-label" htmlFor="inlineCheckbox3">
-              3 (disabled)
-            </label>
-          </div>
+          {this.state.permissions.map((p: PermissionProps) => {
+            return (
+              <div className="form-check form-check-inline">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value={p.id}
+                />
+                <label className="form-check-label">{p.name}</label>
+              </div>
+            );
+          })}
         </form>
       </Wrapper>
     );
