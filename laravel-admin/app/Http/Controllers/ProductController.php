0c592361363beed7b7bd23dea93afadd1375aeb3
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-14 17:12:51 +0500

    [Laravel] | Controller | Product | Gate namespace added

--- laravel-admin/app/Http/Controllers/ProductController.php
+++ laravel-admin/app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\ProductCreateRequest;
 use App\Http\Resources\ProductResource;
 use App\Product;
+use Gate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Storage;
@@ -14,35 +15,35 @@ class ProductController extends Controller
 {
     public function index()
     {
-        Gate::authorize('view','products');
+        Gate::authorize('view', 'products');
         $products = Product::paginate();
         return ProductResource::collection($products);
     }
 
     public function show($id)
     {
-        Gate::authorize('view','products');
+        Gate::authorize('view', 'products');
         return new ProductResource(Product::find($id));
     }
 
     public function store(ProductCreateRequest $request)
     {
-        Gate::authorize('edit','products');
+        Gate::authorize('edit', 'products');
         $product = Product::create($request->only(['title', 'description', 'imageUrl', 'price']));
         return response(new ProductResource($product), Response::HTTP_CREATED);
     }
 
     public function update(Request $request, $id)
     {
-        Gate::authorize('edit','products');
+        Gate::authorize('edit', 'products');
         $product = Product::find($id);
-        $product->update($request->only(['title','description','imageUrl','price']));
+        $product->update($request->only(['title', 'description', 'imageUrl', 'price']));
         return response(new ProductResource($product), Response::HTTP_ACCEPTED);
     }
 
     public function destroy($id)
     {
-        Gate::authorize('edit','products');
+        Gate::authorize('edit', 'products');
         Product::destroy($id);
         return response(null, Response::HTTP_NO_CONTENT);
     }
