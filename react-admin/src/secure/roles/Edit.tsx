e2f65061acbf3a7b3fbbd254e214f0247581eae6
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-14 16:07:56 +0500

    [React] | Role | Edit | data populate

--- react-admin/src/secure/roles/Edit.tsx
+++ react-admin/src/secure/roles/Edit.tsx
@@ -78,7 +78,7 @@ class EditRole extends Component<any, any> {
               className="form-control"
               id="name"
               placeholder="Please Enter Role Name"
-              defaultValue={(this.name = this.state.name)}
+              defaultValue={this.name = this.state.name}
               onChange={(e) => (this.name = e.target.value)}
             />
           </div>
