8ecaa6f528e413d4ca07aa584f34120db67bf1d1
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-15 22:42:58 +0500

    [React | Product  | Create | Layout

---
+++ react-admin/src/secure/products/Create.tsx
@@ -0,0 +1,58 @@
+import { Component } from "react";
+import axios from "axios";
+import Wrapper from "../Wrapper";
+
+class CreateProduct extends Component {
+  title = "";
+  description = "";
+  imageUrl = "";
+  price = "";
+
+  render() {
+    return (
+      <Wrapper>
+        <form>
+          <div className="form-group">
+            <label>Title</label>
+            <input
+              type="text"
+              className="form-control-input"
+              placeholder="Please enter the Product Title"
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Description</label>
+            <textarea
+              className="form-control-input"
+              placeholder="Product Description"
+            ></textarea>
+          </div>
+
+          <div className="form-group">
+            <label>Image</label>
+            <input
+              type="text"
+              className="form-control-input"
+              placeholder="Please enter the Product Title"
+            >
+              <label className="btn">Upload</label>
+              <input type="file" />
+            </input>
+          </div>
+
+          <div className="form-group">
+            <label>Title</label>
+            <input
+              type="text"
+              className="form-control-input"
+              placeholder="Please enter the Product Title"
+            />
+          </div>
+        </form>
+      </Wrapper>
+    );
+  }
+}
+
+export default CreateProduct;
