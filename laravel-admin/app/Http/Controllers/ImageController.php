7a2a190a1e4cdc2a7a7180f7187ec22d94aeb0b9
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-16 05:28:05 +0500

    [Laravel] | Image | Resource is updated

--- laravel-admin/app/Http/Controllers/ImageController.php
+++ laravel-admin/app/Http/Controllers/ImageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ImageUploadRequest;
+use App\Http\Resources\ImageResource;
 use Illuminate\Support\Str;
 use Storage;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,16 +13,15 @@ class ImageController extends Controller
     public function upload(ImageUploadRequest $request)
     {
 
-        if(!$request->has('image'))
-            return response()->json(['message'=>'Please upload the Image...'], Response::HTTP_NOT_FOUND);
-        
+        if (!$request->has('image'))
+            return response()->json(['message' => 'Please upload the Image...'], Response::HTTP_NOT_FOUND);
+
         $file = $request->file('image');
         $name =  Str::random(10); // Random String name
-        $filename = $name.".".$file->extension();
-        $url = Storage::putFileAs("images",$file,$filename);
-        
-        $image_url = env('IMAGE_HOST')."/".$url;
-        return [$image_url]; 
+        $filename = $name . "." . $file->extension();
+        $url = Storage::putFileAs("images", $file, $filename);
 
+        $image_url = env('IMAGE_HOST') . "/" . $url;
+        return new ImageResource($image_url);
     }
 }
--- laravel-admin/app/Http/Resources/ImageResource.php
+++ laravel-admin/app/Http/Resources/ImageResource.php
@@ -14,6 +14,8 @@ class ImageResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+           "url"    =>  $this->imageUrl
+        ]
     }
 }
