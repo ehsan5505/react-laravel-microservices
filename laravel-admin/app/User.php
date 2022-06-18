856b3c0566d68f9b49b648b4f492853f39f4ae20
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-18 23:23:08 +0500

    [Laravel] | User Profile | return the permission as well | debug

--- laravel-admin/app/User.php
+++ laravel-admin/app/User.php
@@ -57,7 +57,7 @@ class User extends Authenticatable
     }
 
     public function permissions(){
-        return $this->role->permissions();
+        return $this->role->permissions;
     }
 
     public function hasAccess($access)
