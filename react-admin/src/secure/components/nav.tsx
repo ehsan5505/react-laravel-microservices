64c2642f0cc57926e85fb0b22eac1270a4796805
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-18 22:11:11 +0500

    [React | Redux | Refactor Profile to use the Redux for user

--- react-admin/src/secure/components/nav.tsx
+++ react-admin/src/secure/components/nav.tsx
@@ -20,8 +20,8 @@ class Nav extends Component<{ user: UserProps }> {
     if (this.state.redirect) return <Navigate to="/login" />;
     return (
       <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-        <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">
-          Company name
+        <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="/">
+          Microstore | Power Up
         </a>
         <button
           className="navbar-toggler position-absolute d-md-none collapsed"
