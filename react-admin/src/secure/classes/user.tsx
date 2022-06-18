bd1c0c1c44be6dd3924dbb86b279f310049d087e
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-18 17:18:23 +0500

    [React | Profile  | Layout and Link in Nav | debug

--- react-admin/src/secure/classes/user.tsx
+++ react-admin/src/secure/classes/user.tsx
@@ -1,13 +1,6 @@
 import Role from "../roles/Role";
 import RoleProps from "./role";
 
-// interface UserProps {
-//   id: number;
-//   first_name: string;
-//   last_name: string;
-//   email: string;
-//   role: RoleProps;
-// }
 
 export default class UserProps {
   id: number;
@@ -17,10 +10,10 @@ export default class UserProps {
   role: RoleProps;
 
   constructor(id = 0, firstname = "", lastname = "", email = "", role:RoleProps = new RoleProps()) {
-      (this.id = id);
-      (this.first_name = firstname;
-      (this.last_name = lastname);
-      (this.email = email);
-      (this.role = role);
+      this.id = id;
+      this.first_name = firstname;
+      this.last_name = lastname;
+      this.email = email;
+      this.role = role;
   }
 }
