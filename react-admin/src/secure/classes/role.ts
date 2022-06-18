7ba0772f55a98ce9e0073cc9df8c4f78984ab3e5
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-18 17:20:07 +0500

    [React | Profile  | Layout and Link in Nav | debug

--- react-admin/src/secure/classes/role.ts
+++ react-admin/src/secure/classes/role.ts
@@ -5,10 +5,10 @@ class RoleProps {
   name: string;
   permission: Permission;
 
-  constructor(id=0,name="",permission=new Permission()){
+  constructor(id = 0, name = "", permission = new Permission()) {
     this.id = id;
     this.name = name;
-    this.permission = permission; 
+    this.permission = permission;
   }
 }
 
