ba53523083f7aac975b8ac8a591cb99a45b6599d
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-18 17:46:04 +0500

    [React | Profile  | Nav | Style

--- react-admin/src/secure/components/nav.tsx
+++ react-admin/src/secure/components/nav.tsx
@@ -52,7 +52,7 @@ class Nav extends Component {
           <div className="nav">
             {/* <div className="nav-item"> */}
             <Link to={"/profile"} className="nav-link text-white">
-              {this.state.user.first_name}
+              {this.state.user.first_name} {this.state.user.last_name}
             </Link>
             <a className="nav-link text-white" onClick={this.signOut}>
               Sign out
