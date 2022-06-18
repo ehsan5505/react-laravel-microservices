988b8831c971dbefc7077bd11f8f0c45431eb25a
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-18 21:49:05 +0500

    [React | Redux | Set Action of User in the Wrapper

--- react-admin/src/secure/Wrapper.tsx
+++ react-admin/src/secure/Wrapper.tsx
@@ -1,14 +1,15 @@
-import React, { Component } from "react";
+import React, { Component, Dispatch, PropsWithChildren } from "react";
 import Menu from "./components/menu";
 import Nav from "./components/nav";
 import axios from "axios";
 import { Navigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-interface WrapperProps {
-  children: React.ReactNode;
-}
+import User from "./users/Users";
+import UserProps from "./classes/user";
+import setUser from "../redux/actions/setUserActions";
+import { connect } from "react-redux";
 
-class Wrapper extends Component<WrapperProps> {
+class Wrapper extends Component<PropsWithChildren<any>> {
   state = {
     redirect: false,
   };
@@ -16,6 +17,7 @@ class Wrapper extends Component<WrapperProps> {
   componentDidMount = async () => {
     try {
       const user = await axios.get("user");
+      this.props.setUser(user.data.data);
     } catch (e) {
       this.setState({ redirect: true });
     }
@@ -43,4 +45,15 @@ class Wrapper extends Component<WrapperProps> {
   }
 }
 
-export default Wrapper;
+const mapStateToProps = (state: { user: UserProps }) => {
+  return {
+    user: state.user,
+  };
+};
+const mapDispatchToProps = (dispatch: Dispatch<any>) => {
+  return {
+    setUser: (user: UserProps) => dispatch(setUser(user)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Wrapper);
