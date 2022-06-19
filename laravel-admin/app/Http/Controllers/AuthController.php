0ba609ef04ab6b8591a1e436bc8fea07db4ee4de
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-19 20:43:30 +0500

    [Laravel] | AuthController | Remove the extend controller

--- laravel-admin/app/Http/Controllers/AuthController.php
+++ laravel-admin/app/Http/Controllers/AuthController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
 
-class AuthController extends Controller
+class AuthController
 {
     // login
     public function login(Request $request)
