0d891f4bdb63d62bb98e6a10d93645ef9763c05f
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-19 22:20:55 +0500

    [Laravel] | User | Drop role_id from users table | debug

--- laravel-admin/database/migrations/2022_06_19_171432_remove_role_id_from_users_table.php
+++ laravel-admin/database/migrations/2022_06_19_171432_remove_role_id_from_users_table.php
@@ -14,7 +14,8 @@ class RemoveRoleIdFromUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropForeign('role_id');
+            Schema::dropColumn('role_id');
         });
     }
 
@@ -26,7 +27,8 @@ class RemoveRoleIdFromUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles');
         });
     }
 }
