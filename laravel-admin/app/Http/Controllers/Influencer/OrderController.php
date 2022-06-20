f0d4e679cbe96ce6a160be5969f21e501babc1f3
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-21 00:30:13 +0500

    [Laravel] | Influencer | Order Controller Update

--- laravel-admin/app/Http/Controllers/Influencer/OrderController.php
+++ laravel-admin/app/Http/Controllers/Influencer/OrderController.php
@@ -2,10 +2,46 @@
 
 namespace App\Http\Controllers\Influencer;
 
-use App\Http\Controllers\Controller;
+use App\Link;
+use App\Order;
+use App\OrderItem;
+use App\Product;
 use Illuminate\Http\Request;
 
-class OrderController extends Controller
+class OrderController
 {
-    //
+    public function store(Request $request)
+    {
+        $link = Link::whereCode($request->input('code'))->first();
+
+        $order = new Order();
+
+        $order->first_name = $request->input('first_name');
+        $order->last_name = $request->input('last_name');
+        $order->email = $request->input('email');
+        $order->code = $link->code;
+        $order->user_id = $link->user->id;
+        $order->influencer_email = $link->user->email;
+        $order->address = $request->input('address');
+        $order->address2 = $request->input('address2');
+        $order->city = $request->input('city');
+        $order->zip = $request->input('zip');
+        $order->country = $request->input('country');
+
+        $order->save();
+
+        foreach ($request->input('items') as $item) {
+            $product = Product::find($item['product_id']);
+
+            $orderItem = new OrderItem();
+            $orderItem->order_id = $order->id;
+            $orderItem->product_title = $product->title;
+            $orderItem->price = $product->price;
+            $orderItem->quantity = $item['quantity'];
+            $orderItem->influencer_revenue = 0.1 * $product->price*$item['quantity'];
+            $orderItem->admin_revenue = 0.9 * $product->price*$item['quantity'];
+
+            $orderItem->save();
+        }
+    }
 }
