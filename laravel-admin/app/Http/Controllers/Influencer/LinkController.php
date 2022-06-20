84eb209bc90d5119c71bdcc14a11fc7cc973b431
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-20 22:16:52 +0500

    [Laravel] | Influencer | Controller | LinkController Updated

--- laravel-admin/app/Http/Controllers/Influencer/LinkController.php
+++ laravel-admin/app/Http/Controllers/Influencer/LinkController.php
@@ -2,10 +2,27 @@
 
 namespace App\Http\Controllers\Influencer;
 
-use App\Http\Controllers\Controller;
+use App\LinkProduct;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
-class LinkController extends Controller
+class LinkController
 {
-    //
+    public function store(Request $request)
+    {
+        $link = Link::create([
+            'user_id' => $request->user()->id,
+            'code'  =>  Str::random(8);
+        ]);
+
+        foreach($request->input('products') as $product_id)
+        {
+            $product_link = LinkProduct::create([
+                'link_id'   =>  $link->id,
+                'product_id'    =>  $product_id
+            ]);
+        }
+
+        return $link;
+    }
 }
