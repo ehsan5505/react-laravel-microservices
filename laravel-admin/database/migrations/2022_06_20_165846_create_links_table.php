7787f05bc1f471069706217f2cae465bb3e22b55
Ehsan <ehsan5505@gmail.com>
2022-06-20 22:00:11 +0500

    [Laravel] | Make Migration for Create Links Table

---
+++ laravel-admin/database/migrations/2022_06_20_165846_create_links_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->id();
+<<<<<<< HEAD:laravel-admin/vendor/laravel/framework/src/Illuminate/Database/Migrations/stubs/migration.create.stub
+=======
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+>>>>>>> e762a9bb52c65e5ca5985a4f53d875d397d11933:laravel-admin/database/migrations/2014_10_12_000000_create_users_table.php
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}
