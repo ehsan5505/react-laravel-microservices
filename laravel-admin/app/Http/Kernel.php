a4f9a61b6e93b2c95c731456b11863bf7584c5ae
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-20 21:03:16 +0500

    [Laravel] | Scope | debug

--- laravel-admin/app/Http/Kernel.php
+++ laravel-admin/app/Http/Kernel.php
@@ -66,5 +66,7 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'scopes' => \Laravel\Passport\Http\Middleware\CheckScopes::class,
         'scope' => \Laravel\Passport\Http\Middleware\CheckForAnyScope::class,
+        'scopes' => \Laravel\Passport\Http\Middleware\CheckScopes::class,
+        'scope' => \Laravel\Passport\Http\Middleware\CheckForAnyScope::class,
     ];
 }
--- laravel-admin/routes/api.php
+++ laravel-admin/routes/api.php
@@ -46,7 +46,7 @@ Route::group(
 // Admin Routes
 Route::group(
     [
-        'middleware'=>  ['auth:api','scopes:admin,influencer'],
+        'middleware'=>  ['auth:api','scopes:admin'],
         'prefix'    =>  'admin',
         'namespace' =>  'Admin',
     ],
