d0c8360184b1aa7b09c4b91dbcd965b991c23fe0
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-27 01:12:46 +0500

    [Laravel] | Stats Controller | Debug

--- laravel-admin/app/User.php
+++ laravel-admin/app/User.php
@@ -83,7 +83,8 @@ class User extends Authenticatable
 
     public function getRevenueAttribute()
     {
-        $orders = Order::where('user_id',$this->id)->where('complete',1)->get();
+        $orders = Order::whereUserId($this->id);
+        // $orders = Order::where('user_id',$this->id)->where('complete',1)->get();
         dd($orders);
         return $orders->sum(function (Order $order){
             return $order->influencer_total;
