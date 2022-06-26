aff1aaa6621c50e01a233d98cf381fa7ab87911b
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-26 21:38:39 +0500

    [Laravel] | Stats | stats Api introduce for the influencer | debug

--- laravel-admin/app/Http/Controllers/Influencer/StatsController.php
+++ laravel-admin/app/Http/Controllers/Influencer/StatsController.php
@@ -11,19 +11,21 @@ class StatsController
 {
     public function index(Request $request)
     {
-        $user = $request->user();
 
-        $links = Link::where('user_id', $user->id)->get();
+        return response(['code' =>'Ehsan' ],200);
+        // $user = $request->user();
 
-        return Response($links->map(function (Link $link) {
-            $orders = Order::where('code', $link->code)->where('complete', 1)->get();
-            return [
-                'code' => $link->code,
-                'count' => $orders->count(),
-                'revenue' => $orders->sum(function (Order $order) {
-                    return $order->influencer_total;
-                })
-            ];
-        }), 200);
+        // $links = Link::where('user_id', $user->id)->get();
+
+        // return Response($links->map(function (Link $link) {
+        //     $orders = Order::where('code', $link->code)->where('complete', 1)->get();
+        //     return [
+        //         'code' => $link->code,
+        //         'count' => $orders->count(),
+        //         'revenue' => $orders->sum(function (Order $order) {
+        //             return $order->influencer_total;
+        //         })
+        //     ];
+        // }), 200);
     }
 }
