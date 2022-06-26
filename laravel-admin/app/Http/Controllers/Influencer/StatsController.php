6c0139d8f6f8c099c532db2117aa3c38b265befd
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-26 21:34:43 +0500

    [Laravel] | Stats | stats Api introduce for the influencer | debug

--- laravel-admin/app/Http/Controllers/Influencer/StatsController.php
+++ laravel-admin/app/Http/Controllers/Influencer/StatsController.php
@@ -12,17 +12,17 @@ class StatsController
     {
         $user = $request->user();
 
-        $links = Link::where('user_id',$user->id)->get();
+        $links = Link::where('user_id', $user->id)->get();
 
-        return $links->map(function(Link $link){
-            $orders = Order::where('code',$link->code)->where('complete',1)->get();
+        return response($links->map(function (Link $link) {
+            $orders = Order::where('code', $link->code)->where('complete', 1)->get();
             return [
                 'code' => $link->code,
                 'count' => $orders->count(),
-                'revenue' => $orders->sum(function(Order $order){
+                'revenue' => $orders->sum(function (Order $order) {
                     return $order->influencer_total;
                 })
             ];
-        });
+        }), 200);
     }
 }
