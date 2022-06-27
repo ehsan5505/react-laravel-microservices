739195ac15e989a23618337547e14f6f59e25a0a
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-27 19:32:13 +0500

    [Laravel] | Redis | Stats | Rankng | Listener | Update Ranking

--- laravel-admin/app/Listeners/RankingListener.php
+++ laravel-admin/app/Listeners/RankingListener.php
@@ -4,8 +4,7 @@ namespace App\Listeners;
 
 use App\Events\OrderCompletedEvent;
 use App\User;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Redis;
 
 class RankingListener
 {
