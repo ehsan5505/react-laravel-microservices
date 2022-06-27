b2ae72cf3b20bf5544806655788ed3ee35b93b60
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-27 17:38:59 +0500

    [Laravel] | Redis | Influence | Product | Cache

--- laravel-admin/app/Http/Controllers/Influencer/ProductController.php
+++ laravel-admin/app/Http/Controllers/Influencer/ProductController.php
@@ -10,13 +10,16 @@ class ProductController
     public function index(Request $request)
     {
 
-        sleep(2);
-        $data = Product::query();
-        if ($query = $request->input('s')) {
-            $data->whereRaw("title LIKE '%${query}%'");
-            $data->OrWhereRaw("description LIKE '%${query}%'");
-        }
-
-        return $data->get();
+        \Cache::remember('products',5,function(){
+            
+            sleep(2);
+            $data = Product::query();
+            if ($query = $request->input('s')) {
+                $data->whereRaw("title LIKE '%${query}%'");
+                $data->OrWhereRaw("description LIKE '%${query}%'");
+            }
+            
+            return $data->get();
+        });
     }
 }
