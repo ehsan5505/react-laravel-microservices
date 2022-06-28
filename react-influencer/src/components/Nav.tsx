e97257f0613fb50ebe338aeeaa7e3469c37ba8a5
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-28 16:06:20 +0500

    [React] | Influencer | Layout diverse in components | debug

--- react-influencer/src/components/Nav.tsx
+++ react-influencer/src/components/Nav.tsx
@@ -41,21 +41,6 @@ const Nav = () => {
       <div className="navbar navbar-dark bg-dark box-shadow">
         <div className="container d-flex justify-content-between">
           <a href="#" className="navbar-brand d-flex align-items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              className="mr-2"
-            >
-              <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path>
-              <circle cx="12" cy="13" r="4"></circle>
-            </svg>
             <strong>Album</strong>
           </a>
           <button
@@ -76,3 +61,4 @@ const Nav = () => {
 };
 
 export default Nav;
+
