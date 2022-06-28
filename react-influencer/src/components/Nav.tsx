04580b526bc7d4a9e9a5c2226918dcffdc56c3a8
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-28 19:05:21 +0500

    [React] | Influencer | Nav update

--- react-influencer/src/components/Nav.tsx
+++ react-influencer/src/components/Nav.tsx
@@ -1,64 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <header>
-      <div className="collapse bg-dark" id="navbarHeader">
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-8 col-md-7 py-4">
-              <h4 className="text-white">About</h4>
-              <p className="text-muted">
-                Add some information about the album below, the author, or any
-                other background context. Make it a few sentences long so folks
-                can pick up some informative tidbits. Then, link them off to
-                some social networking sites or contact information.
-              </p>
-            </div>
-            <div className="col-sm-4 offset-md-1 py-4">
-              <h4 className="text-white">Contact</h4>
-              <ul className="list-unstyled">
-                <li>
-                  <a href="#" className="text-white">
-                    Follow on Twitter
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="text-white">
-                    Like on Facebook
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="text-white">
-                    Email me
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
+    <div className="navbar navbar-dark bg-dark box-shadow">
+      <div className="container d-flex justify-content-between">
+        <Link to={"/"} className="navbar-brand my-0 mr-md auto font-weight-normal">Influencer</Link>
+        <Link to={"/login"} className="btn btn-outline-primary">Login</Link>
       </div>
-      <div className="navbar navbar-dark bg-dark box-shadow">
-        <div className="container d-flex justify-content-between">
-          <a href="#" className="navbar-brand d-flex align-items-center">
-            <strong>Album</strong>
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarHeader"
-            aria-controls="navbarHeader"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-        </div>
-      </div>
-    </header>
+    </div>
   );
 };
 
 export default Nav;
-
