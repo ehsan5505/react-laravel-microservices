5eec2f02489cd66adb2766a0fe1743736988decc
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-28 18:51:33 +0500

    [React] | Influencer | debug Routes

--- react-influencer/src/App.tsx
+++ react-influencer/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Main from "./pages/Main";
 import Login from "./public/Login";
@@ -9,9 +9,11 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
-        <Route path={"/"} element={<Main />} />
-        <Route path={"/login"} element={<Login /> } />
-        <Route path={"/register"} element={<Register />} />
+        <Routes>
+          <Route path={"/"} element={<Main />} />
+          <Route path={"/login"} element={<Login />} />
+          <Route path={"/register"} element={<Register />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
