f02eadd93e94f7b257ff9c11923955d924c098de
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-29 16:59:08 +0500

    [React] | Influencer | Header | debug

--- react-influencer/src/components/Header.tsx
+++ react-influencer/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithRef, ReactNode, useEffect, useState } from "react";
+import React, { PropsWithRef, useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import UserProps from "../classes/user";
