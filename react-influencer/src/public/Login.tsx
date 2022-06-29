938c5b5673faa64cca7a51f9d84ff3fbdd48819a
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-29 06:41:35 +0500

    [React] | Influencer | Nav | debug

--- react-influencer/src/public/Login.tsx
+++ react-influencer/src/public/Login.tsx
@@ -28,6 +28,7 @@ class Login extends Component {
       });
 
       localStorage.setItem('token',resp.data.token);
+      axios.defaults.headers.common['Authorization'] = `Bearer ${resp.data.token}`;
     } catch (err: any) {
       
       this.setState({
