38a9245bb319548257d921e9b81525f06b36317d
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-29 17:36:19 +0500

    [React] | Influencer | Product | populate the info

--- react-influencer/src/pages/Main.tsx
+++ react-influencer/src/pages/Main.tsx
@@ -1,50 +1,51 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { ProductProps } from "../classes/product";
 import Wrapper from "./Wrapper";
 
 const Main = () => {
+  const [products, setProducts] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get(`products?s=${searchText}`);
+      setProducts(response.data.data);
+    })();
+  }, [searchText]);
+
   return (
     <Wrapper>
+      <div className="col-md-12 mb-4">
+        <input type="text" placeholder="Search Products" onKeyUp={e => setSearchText((e.target as HTMLInputElement).value)} />
+      </div>
       <div className="album py-5 bg-light">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-4">
-                <div className="card mb-4 box-shadow">
-                  <img
-                    className="card-img-top"
-                    src="https://via.placeholder.com/100"
-                    alt="Just for avoid warning"
-                  />
-                  <div className="card-body">
-                    <p className="card-text">
-                      This is a wider card with supporting text below as a
-                      natural lead-in to additional content. This content is a
-                      little bit longer.
-                    </p>
-                    <div className="d-flex justify-content-between align-items-center">
-                      <div className="btn-group">
-                        <button
-                          type="button"
-                          className="btn btn-sm btn-outline-secondary"
-                        >
-                          View
-                        </button>
-                        <button
-                          type="button"
-                          className="btn btn-sm btn-outline-secondary"
-                        >
-                          Edit
-                        </button>
+        <div className="container">
+          {products.map((product: ProductProps) => {
+            return (
+              <div className="row" key={product.id}>
+                <div className="col-md-4">
+                  <div className="card mb-4 box-shadow">
+                    <img
+                      className="card-img-top"
+                      src={product.imageUrl}
+                      alt={product.imageUrl}
+                    />
+                    <div className="card-body">
+                      <p className="card-title">{product.title}</p>
+                      <div className="d-flex justify-content-between align-items-center">
+                        <small className="text-muted">{product.price}</small>
                       </div>
-                      <small className="text-muted">9 mins</small>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
+      </div>
     </Wrapper>
-  )
-}
+  );
+};
 
 export default Main;
