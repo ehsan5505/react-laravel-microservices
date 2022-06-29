ea15e8e9f2199a72ca73902ea36282a9e12bf13b
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-29 15:13:05 +0500

    [React] | Influencer | Header | Update

--- react-influencer/src/components/Header.tsx
+++ react-influencer/src/components/Header.tsx
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { PropsWithRef, useState } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import UserProps from "../classes/user";
 
-const Header = () => {
+const Header = (props: PropsWithRef<any>) => {
+  const [title, setTitle] = useState("Welcome");
+  const [description, setDescription] = useState(
+    "Earn upto 10% of the product price by referring"
+  );
+
+  let button;
+
+  if (props.user?.id) {
+    button = (
+      <p>
+        <Link to={"/stats"} className="btn btn-secondary my-2">
+          Statistics
+        </Link>
+      </p>
+    );
+    setTitle("$" + props.user?.revenue);
+    setDescription("Total Amount Earned");
+  } else {
+    <p>
+      <Link to={"/login"} className="btn btn-primary my-2">
+        Login
+      </Link>
+      <Link to={"/register"} className="btn btn-secondary my-2">
+        Register
+      </Link>
+    </p>;
+  }
   return (
     <section className="jumbotron text-center">
-          <div className="container">
-            <h1 className="jumbotron-heading">Album example</h1>
-            <p className="lead text-muted">
-              Something short and leading about the collection below—its
-              contents, the creator, etc. Make it short and sweet, but not too
-              short so folks don't simply skip over it entirely.
-            </p>
-            <p>
-              <a href="#" className="btn btn-primary my-2">
-                Main call to action
-              </a>
-              <a href="#" className="btn btn-secondary my-2">
-                Secondary action
-              </a>
-            </p>
-          </div>
-        </section>
+      <div className="container">
+        <h1 className="jumbotron-heading">{title}</h1>
+        <p className="lead text-muted">{description}</p>
+        {button}
+      </div>
+    </section>
   );
-}
-
-export default Header;
+};
+const mapStateToProps = (state: { user: UserProps }) => {
+  return {
+    user: state.user,
+  };
+};
+export default connect(mapStateToProps)(Header);
