9e41804ba0bc724e6d5a710aa4d751b83203abef
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-06-29 06:22:19 +0500

    [React] | Influencer | Nav | debug

--- react-influencer/src/components/Nav.tsx
+++ react-influencer/src/components/Nav.tsx
@@ -3,39 +3,53 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import UserProps from "../classes/user";
 
-const Nav = (props:PropsWithRef<any>) => {
-
+const Nav = (props: PropsWithRef<any>) => {
   let menu;
 
-  if(props.user){
+  if (props.user) {
     menu = (
       <>
-      <nav className="my-2 my-md-0 mr-md-3">
-        <Link to={"/login"} onClick={() => localStorage.clear()} className="p-2 text-dark">Logout</Link>
-      </nav>
+        <nav className="my-2 my-md-0 mr-md-3">
+          <Link
+            to={"/login"}
+            onClick={() => localStorage.clear()}
+            className="p-2 text-dark"
+          >
+            Logout
+          </Link>
+        </nav>
 
-        <Link to={"/profile"} className="btn btn-outline-primary">{props.user.first_name}</Link>
+        <Link to={"/profile"} className="btn btn-outline-primary">
+          {props.user.first_name}
+        </Link>
       </>
     );
-  }else{
-      menu = (
-        <Link to={"/login"} className="btn btn-outline-primary">Login</Link>
-      );
+  } else {
+    menu = (
+      <Link to={"/login"} className="btn btn-outline-primary">
+        Login
+      </Link>
+    );
   }
 
   return (
-    <div className="navbar navbar-dark bg-dark box-shadow">
-      <div className="container d-flex justify-content-between">
-        <Link to={"/"} className="navbar-brand my-0 mr-md auto font-weight-normal">Influencer</Link>
+    <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 border-bottom shadow-sm">
+      {/* <div className="container d-flex justify-content-between"> */}
+        <Link
+          to={"/"}
+          className="navbar-brand my-0 mr-md auto font-weight-normal"
+        >
+          Influencer
+        </Link>
         {menu}
-      </div>
+      {/* </div> */}
     </div>
   );
 };
 
-const mapStateToProps = (state: {user: UserProps}) => {
+const mapStateToProps = (state: { user: UserProps }) => {
   return {
-    user: state.user
-  }
-}
+    user: state.user,
+  };
+};
 export default connect(mapStateToProps)(Nav);
