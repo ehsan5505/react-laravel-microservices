bb13c1dd1c38fe3fd529afec494a7ac4fdcc9066
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-02 22:45:40 +0500

    [Next] | Checkout | API Integrate | debug

--- next-checkout/pages/[code].tsx
+++ next-checkout/pages/[code].tsx
@@ -34,7 +34,7 @@ const Home = () => {
         <div className="row">
           <div className="col-md-4 order-md-2 mb-4">
             <h4 className="d-flex justify-content-between align-items-center mb-3">
-              <span className="text-muted">Your cart</span>
+              <span className="text-muted">Products</span>
               <span className="badge badge-secondary badge-pill">3</span>
             </h4>
             <ul className="list-group mb-3">
