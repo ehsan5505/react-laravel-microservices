2a571b16a0cd406e841eb753133ae0d29cb6c44a
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-02 21:10:43 +0500

    [Next] | Checkout | layout

--- next-checkout/pages/index.tsx
+++ next-checkout/pages/index.tsx
@@ -1,47 +1,49 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
     <div>
-      <head>
+      <Head>
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
         />
-      </head>
-      <div class="row">
-        <div class="col-md-4 order-md-2 mb-4">
-          <h4 class="d-flex justify-content-between align-items-center mb-3">
-            <span class="text-muted">Your cart</span>
-            <span class="badge badge-secondary badge-pill">3</span>
+      </Head>
+      <div className="row">
+        <div className="col-md-4 order-md-2 mb-4">
+          <h4 className="d-flex justify-content-between align-items-center mb-3">
+            <span className="text-muted">Your cart</span>
+            <span className="badge badge-secondary badge-pill">3</span>
           </h4>
-          <ul class="list-group mb-3">
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
+          <ul className="list-group mb-3">
+            <li className="list-group-item d-flex justify-content-between lh-condensed">
               <div>
-                <h6 class="my-0">Product name</h6>
-                <small class="text-muted">Brief description</small>
+                <h6 className="my-0">Product name</h6>
+                <small className="text-muted">Brief description</small>
               </div>
-              <span class="text-muted">$12</span>
+              <span className="text-muted">$12</span>
             </li>
           </ul>
         </div>
-        <div class="col-md-8 order-md-1">
-          <h4 class="mb-3">Billing address</h4>
-          <form class="needs-validation">
-            <div class="row">
-              <div class="col-md-6 mb-3">
+        <div className="col-md-8 order-md-1">
+          <h4 className="mb-3">Billing address</h4>
+          <form className="needs-validation">
+            <div className="row">
+              <div className="col-md-6 mb-3">
                 <label htmlFor="firstName">First name</label>
                 <input
                   type="text"
-                  class="form-control"
+                  className="form-control"
                   id="firstName"
                   placeholder="First Name"
                   required
                 />
               </div>
-              <div class="col-md-6 mb-3">
+              <div className="col-md-6 mb-3">
                 <label htmlFor="lastName">Last name</label>
                 <input
                   type="text"
-                  class="form-control"
+                  className="form-control"
                   id="lastName"
                   placeholder="Last Name"
                   required
@@ -49,49 +51,49 @@ export default function Home() {
               </div>
             </div>
 
-            <div class="mb-3">
+            <div className="mb-3">
               <label htmlFor="email">
-                Email <span class="text-muted">(Optional)</span>
+                Email <span className="text-muted">(Optional)</span>
               </label>
               <input
                 type="email"
-                class="form-control"
+                className="form-control"
                 id="email"
                 placeholder="you@example.com"
               />
-              <div class="invalid-feedback">
+              <div className="invalid-feedback">
                 Please enter a valid email address for shipping updates.
               </div>
             </div>
 
-            <div class="mb-3">
+            <div className="mb-3">
               <label htmlFor="address">Address</label>
               <input
                 type="text"
-                class="form-control"
+                className="form-control"
                 id="address"
                 placeholder="1234 Main St"
                 required
               />
             </div>
 
-            <div class="mb-3">
+            <div className="mb-3">
               <label htmlFor="address2">
-                Address 2 <span class="text-muted">(Optional)</span>
+                Address 2 <span className="text-muted">(Optional)</span>
               </label>
               <input
                 type="text"
-                class="form-control"
+                className="form-control"
                 id="address2"
                 placeholder="Apartment or suite"
               />
             </div>
 
-            <div class="row">
-              <div class="col-md-5 mb-3">
+            <div className="row">
+              <div className="col-md-5 mb-3">
                 <label htmlFor="country">Country</label>
                 <select
-                  class="custom-select d-block w-100"
+                  className="custom-select d-block w-100"
                   id="country"
                   required
                 >
@@ -100,19 +102,19 @@ export default function Home() {
                 </select>
               </div>
 
-              <div class="col-md-4 mb-3">
+              <div className="col-md-4 mb-3">
                 <label htmlFor="state">State</label>
-                <select class="custom-select d-block w-100" id="state" required>
+                <select className="custom-select d-block w-100" id="state" required>
                   <option value="">Choose...</option>
                   <option>California</option>
                 </select>
               </div>
 
-              <div class="col-md-3 mb-3">
+              <div className="col-md-3 mb-3">
                 <label htmlFor="zip">Zip</label>
                 <input
                   type="text"
-                  class="form-control"
+                  className="form-control"
                   id="zip"
                   placeholder=""
                   required
@@ -120,29 +122,29 @@ export default function Home() {
               </div>
             </div>
 
-            <hr class="mb-4" />
-            <div class="custom-control custom-checkbox">
+            <hr className="mb-4" />
+            <div className="custom-control custom-checkbox">
               <input
                 type="checkbox"
-                class="custom-control-input"
+                className="custom-control-input"
                 id="same-address"
               />
-              <label class="custom-control-label" htmlFor="same-address">
+              <label className="custom-control-label" htmlFor="same-address">
                 Shipping address is the same as my billing address
               </label>
             </div>
-            <div class="custom-control custom-checkbox">
+            <div className="custom-control custom-checkbox">
               <input
                 type="checkbox"
-                class="custom-control-input"
+                className="custom-control-input"
                 id="save-info"
               />
-              <label class="custom-control-label" htmlFor="save-info">
+              <label className="custom-control-label" htmlFor="save-info">
                 Save this information for next time
               </label>
             </div>
-            <hr class="mb-4" />
-            <button class="btn btn-primary btn-lg btn-block" type="submit">
+            <hr className="mb-4" />
+            <button className="btn btn-primary btn-lg btn-block" type="submit">
               Continue to checkout
             </button>
           </form>
