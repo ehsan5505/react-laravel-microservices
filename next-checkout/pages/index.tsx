92ca90650ee643ed2accb9fcc9bc1f9f2282f4e9
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-02 21:48:55 +0500

    [Next] | Checkout | layout | component | tweak

--- next-checkout/pages/index.tsx
+++ next-checkout/pages/index.tsx
@@ -13,139 +13,144 @@ export default function Home() {
           </p>
         </div>
 
-        <div className="col-md-4 order-md-2 mb-4">
-          <h4 className="d-flex justify-content-between align-items-center mb-3">
-            <span className="text-muted">Your cart</span>
-            <span className="badge badge-secondary badge-pill">3</span>
-          </h4>
-          <ul className="list-group mb-3">
-            <li className="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 className="my-0">Product name</h6>
-                <small className="text-muted">Brief description</small>
-              </div>
-              <span className="text-muted">$12</span>
-            </li>
+        <div className="row">
+          <div className="col-md-4 order-md-2 mb-4">
+            <h4 className="d-flex justify-content-between align-items-center mb-3">
+              <span className="text-muted">Your cart</span>
+              <span className="badge badge-secondary badge-pill">3</span>
+            </h4>
+            <ul className="list-group mb-3">
+              <li className="list-group-item d-flex justify-content-between lh-condensed">
+                <div>
+                  <h6 className="my-0">Product name</h6>
+                  <small className="text-muted">Brief description</small>
+                </div>
+                <span className="text-muted">$12</span>
+              </li>
 
-            <li className="list-group-item d-flex justify-content-between">
-              <span>Total (USD)</span>
-              <strong>$20</strong>
-            </li>
-          </ul>
-        </div>
-        <div className="col-md-8 order-md-1">
-          <h4 className="mb-3">Billing address</h4>
-          <form className="needs-validation">
-            <div className="row">
-              <div className="col-md-6 mb-3">
-                <label htmlFor="firstName">First name</label>
+              <li className="list-group-item d-flex justify-content-between">
+                <span>Total (USD)</span>
+                <strong>$20</strong>
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-8 order-md-1">
+            <h4 className="mb-3">Billing address</h4>
+            <form className="needs-validation">
+              <div className="row">
+                <div className="col-md-6 mb-3">
+                  <label htmlFor="firstName">First name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="firstName"
+                    placeholder="First Name"
+                    required
+                  />
+                </div>
+                <div className="col-md-6 mb-3">
+                  <label htmlFor="lastName">Last name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="lastName"
+                    placeholder="Last Name"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="email">
+                  Email <span className="text-muted">(Optional)</span>
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  placeholder="you@example.com"
+                />
+                <div className="invalid-feedback">
+                  Please enter a valid email address for shipping updates.
+                </div>
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="address">Address</label>
                 <input
                   type="text"
                   className="form-control"
-                  id="firstName"
-                  placeholder="First Name"
+                  id="address"
+                  placeholder="1234 Main St"
                   required
                 />
               </div>
-              <div className="col-md-6 mb-3">
-                <label htmlFor="lastName">Last name</label>
+
+              <div className="mb-3">
+                <label htmlFor="address2">
+                  Address 2 <span className="text-muted">(Optional)</span>
+                </label>
                 <input
                   type="text"
                   className="form-control"
-                  id="lastName"
-                  placeholder="Last Name"
-                  required
+                  id="address2"
+                  placeholder="Apartment or suite"
                 />
               </div>
-            </div>
 
-            <div className="mb-3">
-              <label htmlFor="email">
-                Email <span className="text-muted">(Optional)</span>
-              </label>
-              <input
-                type="email"
-                className="form-control"
-                id="email"
-                placeholder="you@example.com"
-              />
-              <div className="invalid-feedback">
-                Please enter a valid email address for shipping updates.
-              </div>
-            </div>
+              <div className="row">
+                <div className="col-md-5 mb-3">
+                  <label htmlFor="country">Country</label>
+                  <select
+                    className="custom-select form-select d-block w-100"
+                    id="country"
+                    required
+                  >
+                    <option value="">Choose...</option>
+                    <option value="IN">India</option>
+                    <option value="PK">Pakistan</option>
+                    <option value="US">United States</option>
+                    <option value="UK">United Kingdom</option>
+                  </select>
+                </div>
 
-            <div className="mb-3">
-              <label htmlFor="address">Address</label>
-              <input
-                type="text"
-                className="form-control"
-                id="address"
-                placeholder="1234 Main St"
-                required
-              />
-            </div>
+                <div className="col-md-4 mb-3">
+                  <label htmlFor="state">State</label>
+                  <select
+                    className="custom-select d-block w-100 form-select"
+                    id="state"
+                    required
+                  >
+                    <option value="">Choose...</option>
+                    <option value="SI">Sindh</option>
+                    <option value="PB">Punjab</option>
+                    <option value="BA">Balochistan</option>
+                    <option value="KP">Khyber Paktuwan</option>
+                    <option value="BL">Balitistan</option>
+                  </select>
+                </div>
 
-            <div className="mb-3">
-              <label htmlFor="address2">
-                Address 2 <span className="text-muted">(Optional)</span>
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="address2"
-                placeholder="Apartment or suite"
-              />
-            </div>
-
-            <div className="row">
-              <div className="col-md-5 mb-3">
-                <label htmlFor="country">Country</label>
-                <select
-                  className="custom-select form-select d-block w-100"
-                  id="country"
-                  required
-                >
-                  <option value="">Choose...</option>
-                  <option value="IN">India</option>
-                  <option value="PK">Pakistan</option>
-                  <option value="US">United States</option>
-                  <option value="UK">United Kingdom</option>
-                </select>
+                <div className="col-md-3 mb-3">
+                  <label htmlFor="zip">Zip</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="zip"
+                    placeholder="Zip"
+                    required
+                  />
+                </div>
               </div>
 
-              <div className="col-md-4 mb-3">
-                <label htmlFor="state">State</label>
-                <select
-                  className="custom-select d-block w-100 form-select"
-                  id="state"
-                  required
-                >
-                  <option value="">Choose...</option>
-                  <option value="SI">Sindh</option>
-                  <option value="PB">Punjab</option>
-                  <option value="BA">Balochistan</option>
-                  <option value="KP">Khyber Paktuwan</option>
-                  <option value="BL">Balitistan</option>
-                </select>
-              </div>
-
-              <div className="col-md-3 mb-3">
-                <label htmlFor="zip">Zip</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="zip"
-                  placeholder="Zip"
-                  required
-                />
-              </div>
-            </div>
-
-            <hr className="mb-4" />
-            <button className="btn btn-primary btn-lg btn-block" type="submit">
-              Continue to checkout
-            </button>
-          </form>
+              <hr className="mb-4" />
+              <button
+                className="btn btn-primary btn-lg btn-block"
+                type="submit"
+              >
+                Continue to checkout
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </Wrapper>
