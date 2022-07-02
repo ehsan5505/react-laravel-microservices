1362bb12ff5b8892321dd38ef478940facbdede1
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-03 01:58:29 +0500

    [Next] | Success | Page | source

--- next-checkout/pages/success.tsx
+++ next-checkout/pages/success.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import Wrapper from "../components/Wrapper";
+import { useRouter } from "../node_modules/next/router";
 
 const Success = () => {
+
+  const router = useRouter();
+  const {source} = router.query;
+  console.info(source);
+
   return (
     <Wrapper>
-    <div className="container">
-      <div className="py-5 text-center">
-        <h2>Success</h2>
-        <p className="lead">Your purchase has been completed!</p>
+      <div className="container">
+        <div className="py-5 text-center">
+          <h2>Success</h2>
+          <p className="lead">Your purchase has been completed!</p>
+        </div>
       </div>
-    </div>
     </Wrapper>
   );
 };
