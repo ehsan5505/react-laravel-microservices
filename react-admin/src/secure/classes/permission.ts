0810dc34a79f196fc356e6847bc91c5b6cfb5757
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-04 23:44:15 +0500

    [Laravel| Dashboard | Menu | User Debug

--- react-admin/src/secure/classes/permission.ts
+++ react-admin/src/secure/classes/permission.ts
@@ -1,9 +1,9 @@
 class PermissionProps {
-  id: number;
+  // id: number;
   name: string;
 
-  constructor(id=0,name=""){
-    this.id = id;
+  constructor(name=""){
+    // this.id = id;
     this.name = name;
   }
 }
