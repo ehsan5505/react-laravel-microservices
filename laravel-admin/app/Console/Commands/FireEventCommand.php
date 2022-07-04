0292f442de4a924f54289b8673cf85c367dce3b7
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-04 21:39:12 +0500

    [Laravel] | Admin | Command | FireEventCommand | trigger the AdminAdded job

--- laravel-admin/app/Console/Commands/FireEventCommand.php
+++ laravel-admin/app/Console/Commands/FireEventCommand.php
@@ -2,41 +2,15 @@
 
 namespace App\Console\Commands;
 
+use App\Jobs\AdminAdded;
 use Illuminate\Console\Command;
 
 class FireEventCommand extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'command:name';
+    protected $signature = 'fire';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
-
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
     public function handle()
     {
-        return 0;
+        return AdminAdded::dispatch();
     }
 }
--- laravel-admin/app/Jobs/AdminAdded.php
+++ laravel-admin/app/Jobs/AdminAdded.php
@@ -12,21 +12,11 @@ class AdminAdded implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    /**
-     * Create a new job instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         //
     }
 
-    /**
-     * Execute the job.
-     *
-     * @return void
-     */
     public function handle()
     {
         //
