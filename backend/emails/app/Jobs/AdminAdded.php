c068572d803c4966655b6ed4750f117731430aed
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-05 01:26:06 +0500

    [Laravel| Admin | AdminAdded | Trigger email via rabbitMQ | debug

--- backend/emails/app/Jobs/AdminAdded.php
+++ backend/emails/app/Jobs/AdminAdded.php
@@ -8,6 +8,8 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Message;
+
 
 class AdminAdded implements ShouldQueue
 {
