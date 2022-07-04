b46fb6096221548b7ce598d1e59676f7da40548d
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-04 22:06:38 +0500

    [Laravel] | Email | Provider | EventServiceProvider | join the AdminAdded job

--- backend/emails/app/Providers/EventServiceProvider.php
+++ backend/emails/app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App;
+use App\Jobs\AdminAdded;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -9,24 +11,10 @@ use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
-    /**
-     * The event listener mappings for the application.
-     *
-     * @var array<class-string, array<int, class-string>>
-     */
-    protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
-    ];
-
-    /**
-     * Register any events for your application.
-     *
-     * @return void
-     */
     public function boot()
     {
-        //
+        App::bindMethod(AdminAdded::class.'@handle', function($job){
+            return $job->handle();
+        });
     }
 }
