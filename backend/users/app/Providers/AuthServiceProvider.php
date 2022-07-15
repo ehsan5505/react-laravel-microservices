3bfa66d9ab22b33869e62b23e09de525dd7382ac
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-15 03:32:06 -0700

    [Laravel/Users| | Passport | configure the passport

--- backend/users/app/Providers/AuthServiceProvider.php
+++ backend/users/app/Providers/AuthServiceProvider.php
@@ -7,24 +7,17 @@ use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
-    protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
-    ];
-
-    /**
-     * Register any authentication / authorization services.
-     *
-     * @return void
-     */
+    
     public function boot()
     {
         $this->registerPolicies();
 
-        //
+        Passport::routes();
+
+        Passport::tokensCan([
+            'admin' =>  "Admin Scope",
+            'influencer' =>  "Influencer Scope"
+        ]);
+
     }
 }
