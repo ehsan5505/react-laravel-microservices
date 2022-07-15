7673d20fe6b8b81a1f7199096b8ee7ec075dc6f4
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-15 03:02:07 -0700

    [Laravel/Users| | Schema | User | Define the users schema

--- backend/users/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/users/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('is_fluencer')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
