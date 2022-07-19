76d88dac0a187d8925fddade16b7d55a76b05879
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-19 11:56:19 -0700

    [User Microservice] | Refactor the AuthController and move to the dedicate microservice | debug

--- backend/users/routes/api.php
+++ backend/users/routes/api.php
@@ -15,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get("test",[AuthController::class,"index"]);
+Route::get("hello", function () {
+    return "Hello World!";
+});
+
 
 Route::post("login", [AuthController::class, "login"]);
 Route::post("logout", [AuthController::class, "logout"]);
