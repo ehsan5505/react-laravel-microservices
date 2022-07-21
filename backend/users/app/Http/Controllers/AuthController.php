b1af4e3ddf9205490b0187439751ac96974a0dc1
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-21 15:55:11 -0700

    [Laravel Microservice] | Auth | User Route | revert the changes

--- backend/users/app/Http/Controllers/AuthController.php
+++ backend/users/app/Http/Controllers/AuthController.php
@@ -28,8 +28,6 @@ class AuthController
                 return response(["error" => "Access Denied"], Response::HTTP_FORBIDDEN);
             }
             
-            dd($user->createToken($scope,[$scope])->accessToken->token);
-            
             $token=$user->createToken($scope,[$scope])->accessToken;
             
             $cookie = cookie('jwt', $token, 3600);
