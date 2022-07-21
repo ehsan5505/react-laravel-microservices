068a7363fa933676db75608c8cfd939de396cbb2
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-21 16:34:03 -0700

    [Laravel Microservice] | Auth | User Route | revert the changes

--- backend/users/app/Models/User.php
+++ backend/users/app/Models/User.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Laravel\Passport\HasApiToken;
 
 /**
  * App\Models\User
