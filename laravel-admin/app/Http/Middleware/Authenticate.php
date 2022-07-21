cecdd3e9f6d68958bb85fbd80ce950761daca7e7
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-21 09:48:01 -0700

    [Laravel Microservice] | Auth | User Route | Debug

--- laravel-admin/app/Http/Middleware/Authenticate.php
+++ laravel-admin/app/Http/Middleware/Authenticate.php
@@ -14,9 +14,9 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
-        }
+        // if (! $request->expectsJson()) {
+        //     return route('login');
+        // }
     }
 
     public function handle($request,Closure $next,...$guards)
