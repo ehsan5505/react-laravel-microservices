4763d8605b579a136c8c82e76c60214002de16ab
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-21 05:37:55 -0700

    [User Microservice] | Debug the login issue

--- backend/users/app/Providers/AuthServiceProvider.php
+++ backend/users/app/Providers/AuthServiceProvider.php
@@ -5,7 +5,6 @@ use Laravel\Passport\Passport;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
-use Laravel\Passport\Passport;
 
 
 class AuthServiceProvider extends ServiceProvider
