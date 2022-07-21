ebbb5aa582a10a9f04581b28cbab6407a2050d71
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-21 14:56:44 -0700

    [Laravel Microservice] | Auth | User Route | revert the changes

--- backend/users/app/Http/Middleware/Authenticate.php
+++ backend/users/app/Http/Middleware/Authenticate.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Middleware;
-
+use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
@@ -17,5 +17,18 @@ class Authenticate extends Middleware
         if (! $request->expectsJson()) {
             return route('login');
         }
+    }
+
+    public function handle($request,Closure $next,...$guards)
+    {
+        // Intercept the Cookie and set it in the header as Authorization 
+        if($token = $request->cookie('jwt')){
+            $request->headers->set('Authorization','Bearer '.$token);
+        }
+        
+        $this->authenticate($request,$guards);
+
+        return $next($request);
+
     }
 }
