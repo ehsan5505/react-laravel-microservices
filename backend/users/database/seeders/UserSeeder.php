bff09710a826fabe2d5e9c697a3c66285f19e1a7
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-21 09:14:07 -0700

    [User Microservice] | Data Migration | User Data Migration

--- backend/users/database/seeders/UserSeeder.php
+++ backend/users/database/seeders/UserSeeder.php
@@ -18,13 +18,10 @@ class UserSeeder extends Seeder
 
         foreach ($users as $user) {
             User::create([
-                "id" =>  $users->id,
                 "first_name"    => $user->first_name,
                 "last_name"     =>  $user->last_name,
                 "email"         =>  $user->email,
                 "password"         =>  $user->password,
-                "created_at"         =>  $user->created_at,
-                "updated_at"         =>  $user->updated_at,
                 "is_fluencer"         =>  $user->is_fluencer
             ]);
         }
