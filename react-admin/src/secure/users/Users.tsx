e5e0140a8845a659297d3d7512c59287d64049a6
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-22 10:07:46 -0700

    [Laravel Admin] | User Controller | get Users not return the role and permission | debug

--- react-admin/src/secure/users/Users.tsx
+++ react-admin/src/secure/users/Users.tsx
@@ -80,7 +80,7 @@ class User extends Component<{ user: UserProps }> {
               </tr>
             </thead>
             <tbody>
-              {this.state.users.map((user: any) => {
+              {this.state.users.map((user: UserProps) => {
                 return (
                   <tr key={user.id}>
                     <td>{user.id}</td>
@@ -88,8 +88,8 @@ class User extends Component<{ user: UserProps }> {
                       {user.first_name} {user.last_name}
                     </td>
                     <td>{user.email}</td>
-                    {/* <td>{user.role.name}</td>
-                    <td>{this.action(user.id)}</td> */}
+                    <td>{user.role.name}</td>
+                    <td>{this.action(user.id)}</td>
                   </tr>
                 );
               })}
