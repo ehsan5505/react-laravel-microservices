4f75a9189057bbdbc20f8511c1169aa867fdd376
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-22 10:16:11 -0700

    [Laravel Admin] | User Controller | get Users not return the role and permission | debug

--- react-admin/src/secure/Wrapper.tsx
+++ react-admin/src/secure/Wrapper.tsx
@@ -18,7 +18,7 @@ class Wrapper extends Component<PropsWithChildren<any>> {
     try {
       const resp = await axios.get(`${constant.BASE_USER}/user`);
       const user: UserProps = resp.data.data;
-      this.props.setUser(new UserProps(user.id,user.first_name,user.last_name,user.email,user.role,user.permissions));
+      this.props.setUser(new UserProps(user.id,user.first_name,user.last_name,user.email,user.roles,user.permissions));
     } catch (e) {
       this.setState({ redirect: true });
     }
