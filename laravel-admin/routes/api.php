ec0cfeb05fbb21c9c0865ad785e4d0a56ae391ba
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-22 07:46:05 -0700

    [React Admin] | Using the Session Token as Authorization | debug

--- laravel-admin/routes/api.php
+++ laravel-admin/routes/api.php
@@ -15,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('user', 'AuthController@user');
 
 
 // Admin Routes
@@ -24,6 +23,7 @@ Route::group([
     'prefix' => 'admin',
     'namespace' => 'Admin',
 ], function () {
+    Route::get('user', 'AuthController@user');
     Route::get('chart', 'DashboardController@chart');
     Route::post('image', "ImageController@upload");
     Route::get("export", "OrderController@export");
