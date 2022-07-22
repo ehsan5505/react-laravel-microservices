432a56695aeb8b4582b5ac1840f264c0bfdfc00a
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-22 10:03:13 -0700

    [Laravel Admin] | User Controller | get Users not return the role and permission | debug

--- laravel-admin/app/Http/Controllers/Admin/UserController.php
+++ laravel-admin/app/Http/Controllers/Admin/UserController.php
@@ -33,8 +33,8 @@ class UserController
     {
         // Did the Role has has access view | users (Gate(privilege, model?))
         $this->userService->allows('view', 'users');
-        // $users = User::whereIsFluencer(0)->paginate();
-        $users = User::paginate();
+        $users = User::whereIsFluencer(0)->paginate();
+        // $users = User::paginate();
         return response($users);
         // return UserResource::collection($users);
     }
