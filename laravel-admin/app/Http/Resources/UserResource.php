47cf1a5e1e1c838ea5e757f3d0e845867dccb154
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-22 10:06:12 -0700

    [Laravel Admin] | User Controller | get Users not return the role and permission | debug

--- laravel-admin/app/Http/Resources/UserResource.php
+++ laravel-admin/app/Http/Resources/UserResource.php
@@ -19,6 +19,7 @@ class UserResource extends JsonResource
             'first_name'    =>  $this->first_name,
             'last_name'     =>  $this->last_name,
             'email'         =>  $this->email,
+            'roles'         =>  $this->role,
         ];
     }
 }
