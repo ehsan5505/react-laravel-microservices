e452c470e75a84f83f3bfbc73b55e411fd87f94a
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-22 06:55:13 -0700

    [Admin Laravel] | Turnoff the Gates

--- react-admin/src/index.tsx
+++ react-admin/src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import axios from "axios";
 import "react-toastify/dist/ReactToastify.css";
 import configureStore from "./redux/configureStore";
