d03abec85301fea8b799e68b42798c786aed0aeb
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-22 09:57:15 -0700

    [React Admin] | User Component | Use the correct url | debug

--- react-admin/src/secure/users/Users.tsx
+++ react-admin/src/secure/users/Users.tsx
@@ -20,7 +20,7 @@ class User extends Component<{ user: UserProps }> {
   componentDidMount = async () => {
     const res = await axios.get(`${constant.BASE_URL}/users?page=${this.page}`);
 
-    console.info(res);
+    console.info(res.data.data);
     this.setState({
       users: res.data.data,
     });
