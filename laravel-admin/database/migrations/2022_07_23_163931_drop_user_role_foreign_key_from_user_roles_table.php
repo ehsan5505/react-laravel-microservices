a1f15568b407dadca0c71b851ac0f905da986f56
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-23 09:55:59 -0700

    [User MicroService] | User Controller | debug

--- laravel-admin/database/migrations/2022_07_23_163931_drop_user_role_foreign_key_from_user_roles_table.php
+++ laravel-admin/database/migrations/2022_07_23_163931_drop_user_role_foreign_key_from_user_roles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class DropUserRoleForeignKeyFromUsersRolesTable extends Migration
+class DropUserRoleForeignKeyFromUserRolesTable extends Migration
 {
     /**
      * Run the migrations.
