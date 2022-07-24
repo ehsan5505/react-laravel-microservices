160c51d5fe8fcd6f3f5a53c131c3467578912a07
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-24 04:43:52 -0700

    [Laravel Microservice| | Update Ranking | debug

--- laravel-admin/app/Console/Commands/UpdateRankingCommand.php
+++ laravel-admin/app/Console/Commands/UpdateRankingCommand.php
@@ -33,7 +33,7 @@ class UpdateRankingCommand extends Command
 
 
         $users->each(function ($user) {
-            dd(`$user`);
+            print_r(`$user`);
             // $orders = Order::where('user_id', $user['id'])->where('complete', 1)->get();
             // $revenue = $orders->sum(function (Order $order) {
             //     return (int) $order->influencer_total;
