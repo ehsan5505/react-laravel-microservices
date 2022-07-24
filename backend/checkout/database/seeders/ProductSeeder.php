2935c3221335193f3fc1abc7ba2ff4b69fed67b8
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-24 15:31:17 -0700

    [Laravel Checkout| | config | DB | Setup for the Migration | DB Seeder Update | debug

--- backend/checkout/database/seeders/ProductSeeder.php
+++ backend/checkout/database/seeders/ProductSeeder.php
@@ -15,18 +15,18 @@ class ProductSeeder extends Seeder
     public function run()
     {
         $products = \DB::connection('mysql_migrate')->table('products')->get();
-        dump($products);
-        // foreach($products as $product)
-        // {
-        //     Product::create([
-        //         'id'        =>  $product['id'],
-        //         'title'     =>  $product['title'],
-        //         'description'=>  $product['description'],
-        //         'imageUrl'  =>  $product['imageUrl'],
-        //         'price'     =>  $product['price'],
-        //         'created_at'=>  $product['created_at'],
-        //         'updated_at'=>  $product['updated_at'],
-        //     ]);
-        // }
+        // dump($products);
+        foreach($products as $product)
+        {
+            Product::create([
+                'id'        =>  $product['id'],
+                'title'     =>  $product['title'],
+                'description'=>  $product['description'],
+                'imageUrl'  =>  $product['imageUrl'],
+                'price'     =>  $product['price'],
+                'created_at'=>  $product['created_at'],
+                'updated_at'=>  $product['updated_at'],
+            ]);
+        }
     }
 }
