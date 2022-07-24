a28431639953ca1eb056a7c998b478dbd3648665
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-24 15:41:23 -0700

    [Laravel Checkout| | config | DB | Setup for the Migration | DB Seeder Update | debug

--- backend/checkout/database/seeders/OrderSeeder.php
+++ backend/checkout/database/seeders/OrderSeeder.php
@@ -18,22 +18,22 @@ class OrderSeeder extends Seeder
         foreach($orders as $order)
         {
             Order::create([
-                'id'        =>  $order['id'],
-                'code'      =>  $order['code'],
-                'first_name'=>  $order['first_name'],
-                'last_name' =>  $order['last_name'],
-                'email'     =>  $order['email'],
-                'user_id'   =>  $order['user_id'],
-                'influencer_email'=>  $order['influencer_email'],
-                'address'   =>  $order['address'],
-                'address2'  =>  $order['address2'],
-                'city'      =>  $order['city'],
-                'country'   =>  $order['country'],
-                'zip'       =>  $order['zip'],
-                'complete'  =>  $order['complete'],
-                'transaction_id'=>  $order['transaction_id'],
-                'created_at'=>  $order['created_at'],
-                'updated_at'=>  $order['updated_at'],
+                'id'        =>  $order->id,
+                'code'      =>  $order->code,
+                'first_name'=>  $order->first_name,
+                'last_name' =>  $order->last_name,
+                'email'     =>  $order->email,
+                'user_id'   =>  $order->user_id,
+                'influencer_email'=>  $order->influencer_email,
+                'address'   =>  $order->address,
+                'address2'  =>  $order->address2,
+                'city'      =>  $order->city,
+                'country'   =>  $order->country,
+                'zip'       =>  $order->zip,
+                'complete'  =>  $order->complete,
+                'transaction_id'=>  $order->transaction_id,
+                'created_at'=>  $order->created_at,
+                'updated_at'=>  $order->updated_at,
             ]);
         }
     }
