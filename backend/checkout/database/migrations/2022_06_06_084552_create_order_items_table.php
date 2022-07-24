6e177b19bb552d1ce50a205e92ed08c216b76055
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-24 14:33:01 -0700

    [Laravel Checkout| | Database and Docker Setup | Deifine

---
+++ backend/checkout/database/migrations/2022_06_06_084552_create_order_items_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->string('title');
+            $table->decimal('price');
+            $table->unsignedInteger('quantity');
+            $table->decimal('influencer_revenue');
+            $table->decimal('admin_revenue');
+            $table->timestamps();
+            
+            // Add the Foreign Key to point to Orders table
+            $table->foreign('order_id')->references('id')->on('orders');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+}
--- backend/checkout/database/migrations/2022_06_20_165846_create_links_table.php
+++ backend/checkout/database/migrations/2022_06_20_165846_create_links_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateLinksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('links', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->primary();
+            $table->unsignedBigInteger('user_id');
+            $table->string('code')->unique();
+            $table->timestamps();
         });
     }
 
@@ -27,6 +28,7 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+
+        Schema::dropIfExists('links');
     }
 }
