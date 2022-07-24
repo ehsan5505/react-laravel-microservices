1a6859f6bf8f0dc6363fd8ea9783ba8e79d3ea4a
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-24 03:57:45 -0700

    [Laravel Microservice| | Update Ranking | debug

--- laravel-admin/app/Console/Commands/UpdateRankingCommand.php
+++ laravel-admin/app/Console/Commands/UpdateRankingCommand.php
@@ -23,7 +23,7 @@ class UpdateRankingCommand extends Command
         
         $userService = new UserService();
         
-        // $users = collect($userService->all(-1));
+        $users = collect($userService->all(-1));
         dd($users);
         // $users = $users->filter(function($user){
         //     return $user->is_fluencer;
