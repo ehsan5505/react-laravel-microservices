eaa90a58f97b1d339db3f5e40d0d70ed632c95ac
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-24 10:27:04 -0700

    [Laravel Admin| | User | User Model Separate migrate to dedicate Microservice  | debug

--- laravel-admin/app/Http/Resources/UserResource.php
+++ laravel-admin/app/Http/Resources/UserResource.php
@@ -19,7 +19,6 @@ class UserResource extends JsonResource
             'first_name'    =>  $this->first_name,
             'last_name'     =>  $this->last_name,
             'email'         =>  $this->email,
-            'roles'         =>  $this->role,
         ];
     }
 }
