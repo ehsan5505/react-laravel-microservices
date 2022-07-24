a014a6d08b59e3c43aab3184dd9d084eaeccd858
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-24 15:39:01 -0700

    [Laravel Checkout| | config | DB | Setup for the Migration | DB Seeder Update | debug

--- backend/checkout/database/seeders/DatabaseSeeder.php
+++ backend/checkout/database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(LinkSeeder::class);
-        $this->call(ProductSeeder::class);
+        $this->call(LinkSeeder::class);
+        // $this->call(ProductSeeder::class);
         // $this->call(OrderSeeder::class);
         // $this->call(OrderItemSeeder::class);
         // $this->call(LinkProductSeeder::class);
