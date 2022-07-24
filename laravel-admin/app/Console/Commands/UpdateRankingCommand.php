1a5deeb9c9c04f61bc5c260847348dd1c0cbf702
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-24 05:35:12 -0700

    [Laravel Microservice| | Update Ranking | debug

--- laravel-admin/app/Console/Commands/UpdateRankingCommand.php
+++ laravel-admin/app/Console/Commands/UpdateRankingCommand.php
@@ -32,7 +32,8 @@ class UpdateRankingCommand extends Command
 
         // dd($users);
 
-        dd(Redis::connection());
+        // dd(Redis::connection());
+        Redis::zadd("rankings","44", "Another Test");
 
 
         // $users->each(function ($user) {
