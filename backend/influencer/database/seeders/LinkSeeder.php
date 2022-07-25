5a72bfae03f487fb2e68e874500641894007c34f
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-25 16:52:34 -0700

    [Laravel | Influencer] | Data Migrate from Admin to Influencer Microcontroller

--- backend/influencer/database/seeders/LinkSeeder.php
+++ backend/influencer/database/seeders/LinkSeeder.php
@@ -11,18 +11,16 @@ class LinkSeeder extends Seeder
     {
         $links = \DB::connection('mysql_migrate')->table('links')->get();
 
-        dump($links);
-
-        // foreach($links as $link)
-        // {
-        //     Link::create([
-        //         "id"        =>  $link->id,
-        //         "user_id"   =>  $link->user_id,
-        //         "code"      =>  $link->code,
-        //         "created_at"=>  $link->created_at,
-        //         "updated_at"=>  $link->updated_at
-        //     ]);
-        // }
+        foreach($links as $link)
+        {
+            Link::create([
+                "id"        =>  $link->id,
+                "user_id"   =>  $link->user_id,
+                "code"      =>  $link->code,
+                "created_at"=>  $link->created_at,
+                "updated_at"=>  $link->updated_at
+            ]);
+        }
 
     }
 }
