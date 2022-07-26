c2669f33b8ce39b3cbb0ed12a572ef73709309ed
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-26 06:42:34 -0700

    [Laravel | Admin] | Data Migrate

--- backend/admin/database/seeders/UserRoleSeeder.php
+++ backend/admin/database/seeders/UserRoleSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\UserRole;
 use Illuminate\Database\Seeder;
 
 class UserRoleSeeder extends Seeder
@@ -9,6 +10,13 @@ class UserRoleSeeder extends Seeder
     public function run()
     {
         $user_roles = \DB::connection('mysql_migrate')->table('user_roles')->get();
-        dump($user_roles);
+
+        foreach ($user_roles as $user_role) {
+            UserRole::create([
+                'id'        => $user_role['id'],
+                'user_id'   => $user_role['user_id'],
+                'role_id'   => $user_role['role_id'],
+            ]);
+        }
     }
 }
