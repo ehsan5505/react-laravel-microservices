7a388e4dc0572ba5c0b76a212e1d790e94a35f70
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-26 06:47:30 -0700

    [Laravel | Admin] | Data Migrate

--- backend/admin/database/seeders/UserRoleSeeder.php
+++ backend/admin/database/seeders/UserRoleSeeder.php
@@ -10,14 +10,12 @@ class UserRoleSeeder extends Seeder
     public function run()
     {
         $user_roles = \DB::connection('mysql_migrate')->table('user_roles')->get();
-
         foreach ($user_roles as $user_role) {
-            dump($user_role);
-            // UserRole::create([
-            //     'id'        => $user_role['id'],
-            //     'user_id'   => $user_role['user_id'],
-            //     'role_id'   => $user_role['role_id'],
-            // ]);
+            UserRole::create([
+                'id'        =>  $user_role->id,
+                'user_id'   =>  $user_role->user_id,
+                'role_id'   =>  $user_role->role_id,
+            ]);
         }
     }
 }
