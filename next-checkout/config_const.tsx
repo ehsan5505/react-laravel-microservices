76cc674f321ab7f138621ad455e97c9144ea20da
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-26 08:34:14 -0700

    MicroService | Config Debug

--- next-checkout/config_const.tsx
+++ next-checkout/config_const.tsx
@@ -1,5 +1,5 @@
 const config = {
-  endpoint: "http://192.168.22.138:8000/api/checkout",
+  endpoint: "http://192.168.22.138:8002/api/checkout",
   stripe_key: "pk_test_51LDywySJz9cExrDXVV7Rx9LSmyE6fPqVfBEDdGFwwijATLIrsT08nBeoaus2cb7Fh7Sg6GHTNLwjBlv7fihBDUyL00544DUVzT"
 }
 export default config;
--- react-influencer/src/config_const.ts
+++ react-influencer/src/config_const.ts
@@ -2,9 +2,9 @@ import { Provider } from "react-redux";
 
 const dev = {
   CHECKOUT_URL: "http://192.168.22.138:3002/",
-  BASE_URL: "http://192.168.22.138:8004/api/influencer",
+  BASE_URL: "http://192.168.22.138:8003/api/influencer",
   USER_URL: "http://192.168.22.138:8001/api",
-  BASE_USER: "http://192.168.22.138:8004/api",
+  BASE_USER: "http://192.168.22.138:8003/api",
 
 };
 
