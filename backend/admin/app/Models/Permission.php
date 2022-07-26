d07945100d1b9af7e6a2cb1c8c9eda471aa924a7
Ehsan <ehsan5505@gmail.com>
2022-07-26 18:37:28 +0500

    Laravel | admin | Models IDE update and generate the UserRole Seeder

--- backend/admin/app/Models/Permission.php
+++ backend/admin/app/Models/Permission.php
@@ -4,6 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Permission
+ *
+ * @property int $id
+ * @property string $name
+ * @method static \Illuminate\Database\Eloquent\Builder|Permission newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Permission newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Permission query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Permission whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Permission whereName($value)
+ * @mixin \Eloquent
+ */
 class Permission extends Model
 {
 
