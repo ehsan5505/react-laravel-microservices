5d97b78cd58a98a47dd64abe971dc11bdd58b5ff
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-07-26 09:15:20 -0700

    MicroService | Admin React | Debugging

--- backend/admin/routes/api.php
+++ backend/admin/routes/api.php
@@ -1,6 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ImageController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
